7d66737f7f0f82fcd99e28c03dceae96b3988232
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2014-03-19 17:33:26 -0700

    added ProjectService

---
+++ src/services/ProjectService.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('mms')
+.factory('ProjectService', ['$q', '$http', 'URLService', 'ElementService', 'ViewService', ProjectService]);
+
+function ProjectService($q, $http, URLService, ElementService, ViewService) {
+    
+    
+}
--- src/services/SiteService.js
+++ src/services/SiteService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mms')
-.factory('SiteService', ['$q', '$http', 'URLService', 'ViewService', SiteService]);
+.factory('SiteService', ['$q', '$http', 'URLService', 'ViewService', 'ProjectService', SiteService]);
 
 /**
  * @ngdoc service
@@ -10,12 +10,13 @@ angular.module('mms')
  * @requires $http
  * @requires mms.URLService
  * @requires mms.ViewService
+ * @requires mms.ProjectService
  * 
  * @description
  * This is a utility service for getting alfresco site information, such as 
  * list of all sites, their categories, documents, projects, tags, etc.
  */
-function SiteService($q, $http, URLService, ViewService) {
+function SiteService($q, $http, URLService, ViewService, ProjectService) {
     var currentSite = 'europa';
     var sites = {};
     var siteDocuments = {};
@@ -50,18 +51,57 @@ function SiteService($q, $http, URLService, ViewService) {
         return currentSite;
     };
 
+    /**
+     * @ngdoc method
+     * @name mms.SiteService#getSite
+     * @methodOf mms.SiteService
+     * 
+     * @description
+     * Gets site information - name, title, categories
+     * @param {string} site The name of site to get.
+     * @returns {Promise} Resolves to the site info object.
+     */
     var getSite = function(site) {
 
     };
 
+    /**
+     * @ngdoc method
+     * @name mms.SiteService#getSites
+     * @methodOf mms.SiteService
+     * 
+     * @description
+     * Gets sites information - name, title, categories for all sites on the server
+     * @returns {Promise} Resolves into array of site info objects.
+     */
     var getSites = function() {
 
     };
 
+    /**
+     * @ngdoc method
+     * @name mms.SiteService#getSiteDocuments
+     * @methodOf mms.SiteService
+     * 
+     * @description
+     * Gets all documents on a site
+     * @param {string} site The site name to get documents for.
+     * @returns {Promise} Resolves into array of document objects.
+     */
     var getSiteDocuments = function(site) {
 
     };
 
+    /**
+     * @ngdoc method
+     * @name mms.SiteService#getSiteProjects
+     * @methodOf mms.SiteService
+     * 
+     * @description
+     * Gets all projects on a site
+     * @param {string} site The site name to get projects for.
+     * @returns {Promise} Resolves into array of project objects.
+     */
     var getSiteProjects = function(site) {
 
     };
