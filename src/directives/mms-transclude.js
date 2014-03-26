833f45d1d63a6fd6008470dd230057978be6fb79
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2014-03-26 13:53:53 -0700

    auto updating nested transclusions!!

--- src/directives/mms-transclude.js
+++ src/directives/mms-transclude.js
@@ -1,24 +1,35 @@
 'use strict';
 
 angular.module('mms')
-.directive('mmsTransclude', ['ElementService', mmsTransclude]);
+.directive('mmsTransclude', ['ElementService', '$compile', mmsTransclude]);
 
-function mmsTransclude(ElementService) {
+function mmsTransclude(ElementService, $compile) {
 
     return {
         restrict: 'E',
-        template: '{{element[property]}}',
+        //template: '<div ng-bind-html="element[property]"></div>',
         scope: {
-            id: '@',
+            eid: '@',
             property: '@'
         },
         //controller: ['$scope', controller]
-        link: function(scope, element, attr) {
-            scope.$watch('id', function(newVal, oldVal) {
+        link: function(scope, element, attrs) {
+            scope.$watch('eid', function(newVal, oldVal) {
                 if (newVal === undefined || newVal === null || newVal === '')
                     return;
-                ElementService.getElement(scope.id).then(function(data) {
+                ElementService.getElement(scope.eid).then(function(data) {
                     scope.element = data;
+                    //var el = $compile(scope.element[scope.property])(scope);
+                    //element.append(el);
+                    element.append(scope.element[scope.property]);
+                    $compile(element.contents())(scope);
+                    scope.$watch('element.' + scope.property, function(n, o) {
+                        element.empty();
+                        //var el = $compile(scope.element[scope.property])(scope);
+                        //element.append(el);
+                        element.append(scope.element[scope.property]);
+                        $compile(element.contents())(scope);
+                    });
                 });
             });
         }
