e798c5d0a86c9d9e78f168f19f073689c2687a25
Raffi P Tikidjian <raffi@jpl.nasa.gov>
2014-04-21 13:56:34 -0700

    Add tree hierarchy viewer

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ui.router', 'mms'])
-  
+angular.module('myApp', ['ui.router', 'mms', 'angularBootstrapNavTree'])
+
   .controller('ElementCtrl', ["$scope", "ElementService", function($scope, ElementService) {
     ElementService.getElement($scope.elementid).then(function(data) {
         $scope.element = data;
@@ -14,7 +14,89 @@ angular.module('myApp', ['ui.router', 'mms'])
             $scope.element = data;
         });
     };
-  }]);
+  }])
+
+  .controller('AbnTestController', function($scope, ElementService, ViewService) {
+
+    $scope.documentid = "_17_0_2_3_407019f_1390507581047_689015_29384";
+    
+    // 1. Iterate over view2view and create an array of all element ids
+    // 2. Call get element ids and create a map of element id -> element name structure
+    // 3. Iterate over view2view and create a map of element id -> element tree node reference
+    
+    ViewService.getDocument($scope.documentid).then(function(data) {
+
+      // Array of all the view element ids
+      var viewElementIds = [];
+
+      // Map of view elements from view id -> tree node object
+      var viewElementIds2TreeNodeMap = {};
+
+      // Map of view element id -> view element name
+      var viewElementIds2NameMap = {};
+
+      // document id is the root the tree heirarchy
+      var rootElementId = data.id;
+
+      // Iterate through all the views in the view2view attribute
+      // view2view is a set of elements with related child views
+      // Note: The JSON format is NOT nested - it uses refrencing
+      for (var i = 0; i < data.view2view.length; i++) {
+
+        var viewId = data.view2view[i].id;
+
+        if (! viewElementIds2NameMap[viewId]) {
+          viewElementIds.push(viewId);
+          viewElementIds2TreeNodeMap[viewId] = [];
+          viewElementIds2NameMap[viewId] = "";
+        }
+          
+        for (var j = 0; j < data.view2view[i].childrenViews.length; j++) {
+            
+          var childViewId = data.view2view[i].childrenViews[j];
+
+          if (! viewElementIds2NameMap[childViewId]) {
+            viewElementIds.push(childViewId);
+            viewElementIds2TreeNodeMap[childViewId] = [];
+            viewElementIds2NameMap[childViewId] = "";
+          }
+        }
+      }
+
+      // Call the get element service and pass in all the elements
+      ElementService.getElements(viewElementIds).then(function(elements) {
+
+        // Fill out all the view names first
+        for (var i = 0; i < elements.length; i++)
+        {
+          viewElementIds2NameMap[elements[i].id] = elements[i].name;
+        }
+
+
+        for (var i = 0; i < data.view2view.length; i++) {
+
+          var viewId = data.view2view[i].id;
+
+          viewElementIds2TreeNodeMap[viewId] = { "label" : viewElementIds2NameMap[viewId], "children" : [] };
+          
+          for (var j = 0; j < data.view2view[i].childrenViews.length; j++) {
+            
+            var childViewId = data.view2view[i].childrenViews[j];
+
+            viewElementIds2TreeNodeMap[viewId].children.push( { "label" : viewElementIds2NameMap[childViewId], "children" : []  } );
+
+          }
+        }
+
+        $scope.my_data = [ viewElementIds2TreeNodeMap[rootElementId] ];
+
+      });
+    });
+      
+    $scope.my_data = [];
+
+  });
+
 
 // Declare module for Froala
 angular.module('Froala', ['ui.router', 'mms'])
