6ac87b8be3c1f12b67710b26f8b5eb1a4e3cc456
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2014-04-22 15:24:13 -0700

    prelim handling of new value specs

--- src/directives/mmsTranscludeName.js
+++ src/directives/mmsTranscludeName.js
@@ -1,20 +1,15 @@
 'use strict';
 
 angular.module('mms')
-.directive('mmsTranscludeName', ['ElementService', '$compile', '$modal', mmsTranscludeName]);
+.directive('mmsTranscludeName', ['ElementService', '$compile', mmsTranscludeName]);
 
-function mmsTranscludeName(ElementService, $compile, $modal) {
+function mmsTranscludeName(ElementService, $compile) {
 
     var mmsTranscludeNameLink = function(scope, element, attrs, mmsViewCtrl) {
-        var modalTemplate = '<div class="modal-header"><h3>Element</h3></div>' +
-                    '<div class="modal-body"><mms-spec eid="{{eid}}" editable-field="name"></mms-spec></div>' +
-                    '<div class="modal-footer"><button class="btn btn-primary" ng-click="close()">Close</button></div>';
-        
         element.click(function(e) {
             if (mmsViewCtrl === null || mmsViewCtrl === undefined)
                 return false;
             mmsViewCtrl.transcludeClicked(scope.eid);
-            //e.stopPropagation();
             return false;
         });
 
--- src/directives/mmsTranscludeVal.js
+++ src/directives/mmsTranscludeVal.js
@@ -4,48 +4,60 @@ angular.module('mms')
 .directive('mmsTranscludeVal', ['ElementService', '$compile', mmsTranscludeVal]);
 
 function mmsTranscludeVal(ElementService, $compile) {
-    var template = '<span ng-repeat="value in element.value">{{value}}</span>';
+
+    var mmsTranscludeValLink = function(scope, element, attrs, mmsViewCtrl) {
+        element.click(function(e) {
+            if (mmsViewCtrl === null || mmsViewCtrl === undefined)
+                return false;
+            mmsViewCtrl.transcludeClicked(scope.eid);
+            //e.stopPropagation();
+            return false;
+        });
+
+        var recompile = function(valueElements) {
+            scope.valueElements = valueElements;
+            var toCompileList = [];
+            for (var i = 0; i < scope.valueElements.length; i++) {
+                var type = scope.valueElements[i].type;
+                if (type === 'LiteralString') {
+                    toCompileList.push('<mms-transclude-vs eid="{{valueElements[' + 
+                    i + '].id}}"></mms-transclude-vs>');
+                } else if (type === 'LiteralBoolean') {
+                    toCompileList.push('{{valueElements[' + i + '].boolean}}');
+                } else if (type === 'LiteralInteger') {
+                    toCompileList.push('{{valueElements[' + i + '].integer}}');
+                } else if (type === 'LiteralReal') {
+                    toCompileList.push('{{valueElements[' + i + '].double}}');
+                }
+            }
+            element.empty();
+            var toCompile = ', '.join(toCompileList);
+            element.append(toCompile);
+            $compile(element.contents())(scope); 
+        };
+
+        scope.$watch('eid', function(newVal, oldVal) {
+            if (newVal === undefined || newVal === null || newVal === '')
+                return;
+            ElementService.getElement(scope.eid).then(function(data) {
+                scope.element = data;
+                var el;
+                ElementService.getElements(data.value).then(recompile); 
+                scope.$watchCollection('element.value', function(n, o) {
+                    ElementService.getElements(scope.element.value).then(recompile);
+                });
+            });
+        });
+    };
+
     return {
         restrict: 'E',
         //template: template,
         scope: {
             eid: '@',
         },
+        require: '?^mmsView',
         //controller: ['$scope', controller]
-        link: function(scope, element, attrs) {
-            scope.$watch('eid', function(newVal, oldVal) {
-                if (newVal === undefined || newVal === null || newVal === '')
-                    return;
-                ElementService.getElement(scope.eid).then(function(data) {
-                    scope.element = data;
-                    var el;
-                    element.empty();
-                    if (scope.element.valueType === "LiteralString") {
-                        var toCompile = ''; //to account for transclusions inside string values
-                        for (var i = 0; i < scope.element.value.length; i++) {
-                            toCompile += '<div>' + scope.element.value[i] + '</div>';
-                        }
-                        element.append(toCompile);
-                        $compile(element.contents())(scope);
-                        //el = $compile(toCompile)(scope);
-                        //element.append(el);
-                        scope.$watchCollection('element.value', function(n, o) {
-                            element.empty();
-                            toCompile = '';
-                            for (var i = 0; i < scope.element.value.length; i++) {
-                                toCompile += '<div>' + scope.element.value[i] + '</div>';
-                            }
-                            element.append(toCompile);
-                            $compile(element.contents())(scope);
-                            //el = $compile(toCompile)(scope);
-                            //element.append(el);
-                        });
-                    } else {
-                        el = $compile(template)(scope);
-                        element.append(el);
-                    }
-                });
-            });
-        }
+        link: mmsTranscludeValLink
     };
 }
