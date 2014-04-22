9933edd9f16bf874d8bd90e9aa278cecdf7bd33c
Raffi P Tikidjian <raffi@jpl.nasa.gov>
2014-04-22 16:59:33 -0700

    add button bar to tree view hierarchy

--- app/js/app.js
+++ app/js/app.js
@@ -21,6 +21,10 @@
       // $scope.documentid = "_17_0_2_3_407019f_1390507581047_689015_29384";
       $scope.documentid = "_17_0_2_3_8660276_1389735483493_203695_64097";
       
+      var tree;
+
+      $scope.my_tree = tree = {};
+
       // 1. Iterate over view2view and create an array of all element ids
       // 2. Call get element ids and create a map of element id -> element name structure
       // 3. Iterate over view2view and create a map of element id -> element tree node reference
@@ -85,6 +89,17 @@
 
         $state.go('view', {viewId: viewId});
 
+      };
+
+      $scope.try_adding_a_branch = function() {
+        var branch = tree.get_selected_branch();
+        return tree.add_branch(branch, {
+          label: 'New Branch',
+          data: {
+            something: 42,
+            "else": 43
+          }
+        });
       };
 
     })
