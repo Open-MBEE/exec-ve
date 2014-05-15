9a3b0c84c798a986ab92eb470f57c0fd1c56004d
Benjamin Y Inada <benjamin.y.inada@jpl.nasa.gov>
2014-05-15 09:28:53 -0700

    Inserts un-templified sample accordion menu into Other Sites drop down.

--- src/directives/mmsNav.js
+++ src/directives/mmsNav.js
@@ -24,6 +24,26 @@ function mmsNav(SiteService, $templateCache) {
             }
             scope.categories = sites;
         });
+
+        angular.element("#accordian a").click(function(){
+            var lnk = angular.element(this);
+            var closest_ul = lnk.closest("ul");
+            var parallel_active_links = closest_ul.find(".active");
+            var closest_li = lnk.closest("li");
+            var lnk_status = closest_li.hasClass("active");
+            var count = 0;
+
+            closest_ul.find("ul").slideUp(function(){
+                if(++count == closest_ul.find("ul").length)
+                    parallel_active_links.removeClass("active");
+            });
+
+            if(!lnk_status)
+            {
+                closest_li.children("ul").slideDown();
+                closest_li.addClass("active");
+            }
+        });
     };
 
     return {
