a059216f4fd4f96d55dfcacfae07816bbf89042f
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2014-06-16 12:26:44 -0700

    update timestamp things and test project

--- app/js/ve/controllers.js
+++ app/js/ve/controllers.js
@@ -3,14 +3,14 @@
 /* Controllers */
 
 angular.module('myApp')
-.controller('NavTreeCtrl', ['$scope', '$state', '$stateParams', 'snapshots', 'site', 'ElementService', 'ViewService', 'ConfigService', 'growl',
-function($scope, $state, $stateParams, snapshots, site, ElementService, ViewService, ConfigService, growl) {
+.controller('NavTreeCtrl', ['$scope', '$state', '$stateParams', 'snapshots', 'site', 'time', 'ElementService', 'ViewService', 'ConfigService', 'growl',
+function($scope, $state, $stateParams, snapshots, site, time, ElementService, ViewService, ConfigService, growl) {
     $scope.documentid = $stateParams.docId;
     $scope.snapshots = snapshots;
     $scope.site = site;
-
+    $scope.time = time;
     $scope.createNewSnapshot = function() {
-        ConfigService.createSnapshot($scope.documentid, $scope.site.name, 'master')
+        ConfigService.createSnapshot($scope.documentid)
         .then(function(result) {
             growl.success("Create Successful: wait for email.");
         }, function(reason) {
@@ -24,7 +24,7 @@ function($scope, $state, $stateParams, snapshots, site, ElementService, ViewServ
       // 2. Call get element ids and create a map of element id -> element name structure
       // 3. Iterate over view2view and create a map of element id -> element tree node reference
       
-    ViewService.getDocument($scope.documentid)
+    ViewService.getDocument($scope.documentid, false, 'master', time)
     .then(function(data) {
 
         // Array of all the view element ids
@@ -70,7 +70,7 @@ function($scope, $state, $stateParams, snapshots, site, ElementService, ViewServ
         };
 
         // Call the get element service and pass in all the elements
-        ElementService.getElements(viewElementIds)
+        ElementService.getElements(viewElementIds, false, 'master', time)
         .then(function(elements) {
 
           // Fill out all the view names first
