7b04dabecf89855f7ea195ee04d5d889c79c66ab
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2014-06-17 12:56:01 -0700

    added editable check to buttons, enable getting snapshots

--- app/js/ve/app.js
+++ app/js/ve/app.js
@@ -19,8 +19,8 @@ angular.module('myApp', ['ui.router', 'mms', 'mms.directives', 'fa.directive.bor
                 return $stateParams.time;
             },
             snapshots: function($stateParams, ConfigService) {
-                return [];
-                //return ConfigService.getProductSnapshots($stateParams.docId, $stateParams.site, 'master');
+                //return [];
+                return ConfigService.getProductSnapshots($stateParams.docId, $stateParams.site, 'master');
             }
         },
         views: {
