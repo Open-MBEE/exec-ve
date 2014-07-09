174bc0736c6e918a918556e72b906bb07766c3b5
Christopher C Muschek <Christopher.C.Muschek@jpl.nasa.gov>
2014-07-08 17:26:50 -0700

    Service testing template created

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -13,46 +13,211 @@ describe('service', function() {
   });
 });
 
-// CommentService
+// CommentService - (done), [4 empty]
 describe('CommentService', function() {
 	beforeEach(module('mms'));
 
 	it('can get an instance of the CommentService', inject(function() {
 		expect(CommentService).toBeDefined();
-	}));
 
-	it('should have an addComment function', inject(function() {
 		expect(CommentService.addComment).not.toBe(null);
-	}));
-
-	it('should have a getComments function', inject(function() {
 		expect(CommentService.getComments).not.toBe(null);
-	}));
-
-	it('should have a updateComment function', inject(function() {
 		expect(CommentService.updateComment).not.toBe(null);
+		expect(CommentService.deleteComment).not.toBe(null);
 	}));
 
-	it('should have a deleteComment function', inject(function() {
-		expect(CommentService.deleteComment).not.toBe(null);
+	it('addComment', inject(function() {}));
+
+	it('getComments', inject(function() {}));
+
+	it('updateComment', inject(function() {}));
+
+	it('deleteComment', inject(function() {}));
+});
+
+// ConfigService - incomplete, 10 methods, [10 use $http]
+describe('ConfigService', function() {
+	beforeEach(module('mms'));
+
+	var workspace = 'master';
+
+	var Service;
+
+	it('can get an instance of the ConfigService', inject(function() {
+		expect(ConfigService).toBeDefined();
+
+		Service = ConfigService();
+		expect(Service.getSiteConfigs).not.toBe(null);
+		expect(Service.getConfig).not.toBe(null);
+		expect(Service.getConfigProducts).not.toBe(null);
+		expect(Service.getConfigProducts).not.toBe(null);
+		expect(Service.getConfigSnapshots).not.toBe(null);
+		expect(Service.updateConfig).not.toBe(null);
+		expect(Service.createConfig).not.toBe(null);
+		expect(Service.updateConfigSnapshots).not.toBe(null);
+		expect(Service.updateConfigProducts).not.toBe(null);
+		expect(Service.createSnapshots).not.toBe(null);
+	}));
+
+	// accesses $http - untested
+	it('getSiteConfigs', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getConfig', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getConfigProducts', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getConfigSnapshots', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getProductSnapshots', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('updateConfig', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('createConfig', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('updateConfigSnapshots', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('updateConfigProducts', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('createSnapshots', inject(function() {
+
 	}));
 });
 
-// ConfigService
-
-// ElementService
+// ElementService - incomplete, 12 methods, [1 empty, 10 use $http, 1 other]
 describe('ElementService', function() {
 	beforeEach(module('mms'));
 
+	var Service;
+
 	it('can get an instance of the ElementService and methods are valid', inject(function() {
 		expect(ElementService).toBeDefined();
-		expect(ElementService.getElements).not.toBe(null);
+
+		Service = ElementService();
+		expect(Service.getElement).not.toBe(null);
+		expect(Service.getElements).not.toBe(null);
+		expect(Service.getElementsForEdit).not.toBe(null);
+		expect(Service.getOwnedElements).not.toBe(null);
+		expect(Service.updateElement).not.toBe(null);
+		expect(Service.updateElements).not.toBe(null);
+		expect(Service.createElement).not.toBe(null);
+		expect(Service.createElements).not.toBe(null);
+		expect(Service.getGenericElements).not.toBe(null);
+		expect(Service.isDirty).not.toBe(null);
+		expect(Service.search).not.toBe(null);
+	}));
+
+	// accesses $http - untested
+	it('getElement', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getElements', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getElementForEdit', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getElementsForEdit', inject(function() {
+
+	}));
+
+	// is an empty function
+	it('getOwnedElements', inject(function() {}));
+
+	// accesses $http - untested
+	it('updateElement', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('updateElements', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('createElement', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('createElements', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getGenericElements', inject(function() {
+
+	}));
+
+	// needs clever way to test
+	it('isDirty', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('search', inject(function() {
+
 	}));
 });
 
-// NotificationService
+// NotificationService - (done), 3 methods, [3 empty]
+describe('NotificationService', function() {
+	beforeEach(module('mms'));
 
-// ProjectService
+	it('can get an instance of the NotificationService and methods are valid', inject(function() {
+		expect(NotificationService).toBeDefined();
+
+		expect(NotificationService.getFollowing).not.toBe(null);
+		expect(NotificationService.follow).not.toBe(null);
+		expect(NotificationService.unfollow).not.toBe(null);
+	}));
+
+	it('getFollowing', inject(function() {
+
+	}));
+
+	it('follow', inject(function() {
+
+	}));
+
+	it('unfollow', inject(function() {
+
+	}));
+});
+
+// ProjectService - (done), [empty]
 describe('ProjectService', function() {
 	beforeEach(module('mms'));
 
@@ -62,11 +227,76 @@ describe('ProjectService', function() {
 	}))
 });
 
-// SearchService
+// SearchService - incomplete, 1 method, [1 uses $http]
+describe('SearchService', function() {
+	beforeEach(module('mms'));
 
-// SiteService
+	it('can get an instance of the SearchService and methods are valid', inject(function() {
+		expect(SearchService).toBeDefined();
 
-// URLService
+		expect(SearchService.searchElements).not.toBe(null);
+	}));
+
+	// accesses $http - untested
+	it('searchElements', inject(function() {
+
+	}));
+});
+
+// SiteService - incomplete, 7 methods, [2 use $http, 4 tested, 1 empty]
+describe('SiteService', function() {
+	beforeEach(module('mms'));
+
+	var Service;
+
+	it('can get an instance of the SiteService and methods are valid', inject(function() {
+		expect(SiteService).toBeDefined();
+
+		Service = SiteService();
+		expect(Service.getCurrentSite).not.toBe(null);
+		expect(Service.setCurrentSite).not.toBe(null);
+		expect(Service.getCurrentWorkspace).not.toBe(null);
+		expect(Service.setCurrentWorkspace).not.toBe(null);
+		expect(Service.getSites).not.toBe(null);
+		expect(Service.getSite).not.toBe(null);
+		expect(Service.getSiteProjects).not.toBe(null);
+	}));
+
+	it('getCurrentSite', inject(function() {
+		expect(Service.getCurrentSite()).toBe('europa');
+	}));
+
+	it('setCurrentSite', inject(function() {
+		Service.setCurrentSite('notEuropa');
+		expect(Service.getCurrentSite()).toBe('notEuropa');
+	}));
+
+	it('getCurrentWorkspace', inject(function() {
+		expect(Service.getCurrentWorkspace()).toBe('master');
+	}));
+
+	it('setCurrentWorkspace', inject(function() {
+		Service.setCurrentWorkspace('notMaster');
+		expect(Service.getCurrentWorkspace()).toBe('notMaster');
+	}))
+
+	// accesses $http - untested
+	it('getSites', inject(function() {
+
+	}));
+
+	// accesses $http - untested
+	it('getSite', inject(function() {
+
+	}));
+
+	// empty function
+	it('getSiteProjects', inject(function() {
+
+	}))
+});
+
+// URLService - (done), 16 methods, [16 normal]
 describe('URLService', function() {
 	beforeEach(module('mms'));
 
@@ -186,9 +416,33 @@ describe('URLService', function() {
 	*/
 });
 
-// UtilsService
+// UtilsService - incomplete, 2 methods, [2 other]
+describe('UtilsService', function() {
+	beforeEach(module('mms'));
 
-// VersionService
+	var Service;
+
+	it('can get an instance of the UtilsService and methods are valid', inject(function() {
+		expect(UtilsService).toBeDefined();
+
+		Service = UtilsService();
+		expect(Service.hasCircularReference).not.toBe(null);
+		expect(Service.cleanElement).not.toBe(null);
+	}));
+
+	// will need to come up with way to test this
+	it('hasCircularReference', inject(function() {
+
+	}));
+
+	// will also need to come up with way to test this
+	it('cleanElement', inject(function() {
+
+	}));
+
+});
+
+// VersionService - incomplete, 4 methods, [4 use $http]
 describe('VersionService', function() {
 	beforeEach(module('mms'));
 
@@ -208,14 +462,153 @@ describe('VersionService', function() {
 		expect(Service.getGenericElements).toBeDefined();
 	}));
 
+	// uses $http 
 	it('getElement', inject(function() {
-		var response;
-		//var getElementPromise = Service.getElement(id, 'latest', 'master');
+		
+	}));
 
-		console.log(response);
+	// uses $http
+	it('getElements', inject(function() {
+
+	}));
+
+	// uses $http
+	it('getElementVersions', inject(function() {
+
+	}));
+
+	// uses $http
+	it('getGenericElements', inject(function() {
+
 	}));
 });
 
-// ViewService
+// ViewService - incomplete, 18 methods, [4 empty,  4 tested, 10 use $http]
+describe('ViewService', function() {
+	beforeEach(module('mms'));
 
-// VizService
+	var Service;
+
+	it('can get an instance of the ViewService and methods are valid', inject(function() {
+		expect(ViewService).toBeDefined();
+
+		Service = ViewService();
+		expect(Service.getView).not.toBe(null);
+		expect(Service.getViews).not.toBe(null);
+		expect(Service.getDocument).not.toBe(null);
+		expect(Service.updateView).not.toBe(null);
+		expect(Service.updateDocument).not.toBe(null);
+		expect(Service.getViewElements).not.toBe(null);
+		expect(Service.getViewComments).not.toBe(null);
+		expect(Service.addViewComment).not.toBe(null);
+		expect(Service.deleteViewComment).not.toBe(null);
+		expect(Service.updateViewElements).not.toBe(null);
+		expect(Service.createView).not.toBe(null);
+		expect(Service.addViewToDocument).not.toBe(null);
+		expect(Service.getDocumentViews).not.toBe(null);
+		expect(Service.getSiteDocuments).not.toBe(null);
+		expect(Service.setCurrentViewId).not.toBe(null);
+		expect(Service.setCurrentDocumentId).not.toBe(null);
+		expect(Service.getCurrentViewId).not.toBe(null);
+		expect(Service.getCurrentDocumentId).not.toBe(null);
+	}));
+
+	// calls on ElementService and therefore $http
+	it('getView', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('getViews', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('getDocument', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('updateView', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('updateDocument', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('getViewElements', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('getDocumentViews', inject(function() {
+
+	}));
+
+	// empty function
+	it('getViewComments', inject(function() {}));
+
+	// empty function
+	it('addViewComment', inject(function() {}));
+
+	// empty function
+	it('deleteViewComment', inject(function() {}));
+
+	// empty function
+	it('updateViewElements', inject(function() {}));
+
+	// calls on getDocument and therefore $http
+	it('addViewToDocument', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('createView', inject(function() {
+
+	}));
+
+	// calls on ElementService and therefore $http
+	it('getSiteDocuments', inject(function() {
+
+	}));
+
+	it('getCurrentViewId', inject(function() {
+		expect(Service.getCurrentViewId()).toBe('');
+	}));
+
+	it('setCurrentViewId', inject(function() {
+		Service.setCurrentViewId('newViewId');
+		expect(Service.getCurrentViewId()).toBe('newViewId');
+	}));
+
+	it('getCurrentDocumentId', inject(function() {
+		expect(Service.getCurrentDocumentId()).toBe('');
+	}));
+
+	it('setCurrentDocumentId', inject(function() {
+		Service.setCurrentDocumentId('newDocumentId');
+		expect(Service.getCurrentDocumentId()).toBe('newDocumentId');
+	}))
+});
+
+// VizService - incomplete, 1 method, [1 uses $http]
+describe('VizService', function() {
+	beforeEach(module('mms'));
+
+	var Service;
+
+	it('can get an instance of the VizService and methods are valid', inject(function() {
+		expect(VizService).toBeDefined();
+
+		Service = VizService();
+		expect(Service.getImageURL).not.toBe(null);
+	}));
+
+	// uses $http
+	it('getImageURL', inject(function() {
+
+	}));
+});
