f14c1e1b7d04af598a100c8b2bd9f1c895b8debd
Benjamin Y Inada <benjamin.y.inada@jpl.nasa.gov>
2014-07-28 10:57:56 -0700

    Structures panes.

--- app/js/mm/app.js
+++ app/js/mm/app.js
@@ -1,6 +1,20 @@
 'use strict';
 
-angular.module('mm', ['ui.router', 'mms', 'mms.directives', 'ui.bootstrap', 'ui.tree'])
-    .config(function($stateProvider, $urlRouterProvider) {
-
+angular.module('mm', ['ui.router', 'mms', 'mms.directives', 'ui.bootstrap', 'ui.tree', 'fa.directive.borderLayout'])
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('diff', {
+        url: '/diff',
+        views: {
+            'menu': {
+                template: '<mms-nav mms-type="Model Manager" mms-go-to="false" mms-other-sites="false"></mms-nav>'
+            },
+            'main-view': {
+                templateUrl: 'partials/mm/main-view.html'
+            },
+            'tree-view': {
+                templateUrl: 'partials/mm/tree-view.html'
+            }
+        }
     });
+});
