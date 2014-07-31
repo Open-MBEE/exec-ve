9cd99e4f448c4a14318fd70102c9a2d8a281360a
Benjamin Y Inada <benjamin.y.inada@jpl.nasa.gov>
2014-07-31 10:48:30 -0700

    Switches from using controllers to links.

--- src/directives/mmsDiffTable.js
+++ src/directives/mmsDiffTable.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('mms.directives')
-.directive('mmsDiffTable', ['$templateCache', 'DiffService', mmsDiffTable]);
+.directive('mmsDiffTable', ['$templateCache', '$rootScope', 'DiffService', mmsDiffTable]);
 
-function mmsDiffTable($templateCache, DiffService) {
+function mmsDiffTable($templateCache, $rootScope, DiffService) {
   
-  var MMSDiffTableController = function($scope, $rootScope) {
+  var MMSDiffTableLink = function(scope, element, attrs) {
     // Diff the two workspaces picked in the Workspace Picker
     var response = DiffService.diff('ws1', 'ws2');
     var originals = response.workspace1.elements;
@@ -13,77 +13,75 @@ function mmsDiffTable($templateCache, DiffService) {
 
     // Initializations
     $rootScope.tableElement = null;
-    $scope.original = {};
-    $scope.delta = {};
+    scope.original = {};
+    scope.delta = {};
 
     // Watch for user selections in the containment tree
     $rootScope.$watch('tableElement', function() {
       if ($rootScope.tableElement !== null) {
-        $scope.original.name = $rootScope.tableElement.name;
-        $scope.original.owner = $rootScope.tableElement.owner;
+        scope.original.name = $rootScope.tableElement.name;
+        scope.original.owner = $rootScope.tableElement.owner;
 
-        // TODO - set $scope.delta with proper values
+        // TODO - set scope.delta with proper values
       }
     });
 
-    $scope.differenceTypes = [];
-    $scope.nameDiff = [];
-    $scope.ownerDiff = [];
+    scope.differenceTypes = [];
+    scope.nameDiff = [];
+    scope.ownerDiff = [];
 
-    this.generateDifferenceTypes($scope, deltas);
+    generateDifferenceTypes(scope, deltas);
   };
 
-  MMSDiffTableController.prototype = {
-    generateDifferenceTypes: function(scope, deltas) {
-      if (deltas.updatedElements.length > 0) {
-        scope.differenceTypes.push('Update');
-      }
-
-      if (deltas.addedElements.length > 0) {
-        scope.differenceTypes.push('Addition');
-      }
-
-      if (deltas.deletedElements.length > 0) {
-        scope.differenceTypes.push('Deletion');
-      }
-
-      if (deltas.movedElements.length > 0) {
-        scope.differenceTypes.push('Move');
-      }
-
-      if (deltas.conflicts.length > 0) {
-        scope.differenceTypes.push('Conflict');
-      }
-    },
-
-    /*
-     * Fills scope booleans for two elements.
-     *
-     * scope.*Diff[0] is True, if element is in clean state. False, otherwise.
-     * scope.*Diff[1] is True, if element is in addition state. False, otherwise.
-     * scope.*Diff[2] is True, if element is in removal state. False, otherwise.
-     * scope.*Diff[3] is True, if element is in conflict state. False, otherwise.
-     */
-    /* TODO
-    generateTableClasses: function(original, delta) {
-      this.scope.nameDiff[0] = (this.scope.name[0] === this.scope.name[1]);
-      this.scope.nameDiff[1] = (this.scope.name[0] === '') && (this.scope.name[1].length > 0);
-      this.scope.nameDiff[2] = (this.scope.name[1] === '') && (this.scope.name[0].length > 0);
-      this.scope.nameDiff[3] = false;
-
-      this.scope.ownerDiff[0] = (this.scope.owner[0] === this.scope.owner[1]);
-      this.scope.ownerDiff[1] = (this.scope.owner[0] === '') && (this.scope.owner[1].length > 0);
-      this.scope.ownerDiff[2] = (this.scope.owner[1] === '') && (this.scope.owner[0].length > 0);
-      this.scope.ownerDiff[3] = false;
+  var generateDifferenceTypes = function(scope, deltas) {
+    if (deltas.updatedElements.length > 0) {
+      scope.differenceTypes.push('Update');
     }
-    */
+
+    if (deltas.addedElements.length > 0) {
+      scope.differenceTypes.push('Addition');
+    }
+
+    if (deltas.deletedElements.length > 0) {
+      scope.differenceTypes.push('Deletion');
+    }
+
+    if (deltas.movedElements.length > 0) {
+      scope.differenceTypes.push('Move');
+    }
+
+    if (deltas.conflicts.length > 0) {
+      scope.differenceTypes.push('Conflict');
+    }
   };
+
+  /*
+   * Fills scope booleans for two elements.
+   *
+   * scope.*Diff[0] is True, if element is in clean state. False, otherwise.
+   * scope.*Diff[1] is True, if element is in addition state. False, otherwise.
+   * scope.*Diff[2] is True, if element is in removal state. False, otherwise.
+   * scope.*Diff[3] is True, if element is in conflict state. False, otherwise.
+   */
+  /* TODO
+  generateTableClasses: function(original, delta) {
+    this.scope.nameDiff[0] = (this.scope.name[0] === this.scope.name[1]);
+    this.scope.nameDiff[1] = (this.scope.name[0] === '') && (this.scope.name[1].length > 0);
+    this.scope.nameDiff[2] = (this.scope.name[1] === '') && (this.scope.name[0].length > 0);
+    this.scope.nameDiff[3] = false;
+
+    this.scope.ownerDiff[0] = (this.scope.owner[0] === this.scope.owner[1]);
+    this.scope.ownerDiff[1] = (this.scope.owner[0] === '') && (this.scope.owner[1].length > 0);
+    this.scope.ownerDiff[2] = (this.scope.owner[1] === '') && (this.scope.owner[0].length > 0);
+    this.scope.ownerDiff[3] = false;
+  }
+  */
 
   var MMSDiffTableTemplate = $templateCache.get('mms/templates/mmsDiffTable.html');
 
   return {
     restrict: 'E',
     template: MMSDiffTableTemplate,
-    controller: ['$scope', '$rootScope', 'DiffService' , MMSDiffTableController]
+    link: MMSDiffTableLink
   };
 }
--- src/directives/mmsDiffTree.js
+++ src/directives/mmsDiffTree.js
@@ -4,30 +4,30 @@ angular.module('mms.directives')
 .directive('mmsDiffTree', ['$templateCache', 'DiffService', mmsDiffTree]);
 
 function mmsDiffTree($templateCache, DiffService) {
-  
-  var MMSDiffTreeController = function($scope) {
+
+  var MMSDiffTreeLink = function (scope, element, attrs) {
     // Diff the two workspaces picked in the Workspace Picker
     var response = DiffService.diff('ws1', 'ws2');
     var originals = response.workspace1.elements;
     var deltas = response.workspace2;
 
     // Populate the left pane containment tree
-    $scope.elementNames = [];
+    scope.elementNames = [];
     angular.forEach(originals, function(value, key) {
-      $scope.elementNames.push(value.name);
+      scope.elementNames.push(value.name);
     });
 
     // Proxy function called with ngClick in template
-    $scope.loadTableWithElement = function(element) {
-      DiffService.loadTableWithElement(element);
+    scope.loadTableWithElement = function(elem) {
+      DiffService.loadTableWithElement(elem);
     };
   };
-
+  
   var MMSDiffTreeTemplate = $templateCache.get('mms/templates/mmsDiffTree.html');
 
   return {
     restrict: 'E',
     template: MMSDiffTreeTemplate,
-    controller: ['$scope', MMSDiffTreeController]
+    link: MMSDiffTreeLink
   };
 }
