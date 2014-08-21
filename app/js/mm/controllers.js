41a1789bd8f59c449686f687cca1f8fcf5b19779
Raffi P Tikidjian <raffi@jpl.nasa.gov>
2014-08-20 17:15:34 -0700

    clean-up data-structures for storing changes, show differences in theirs vs. mine properties

--- app/js/mm/controllers.js
+++ app/js/mm/controllers.js
@@ -1,320 +1,201 @@
 'use strict';
 
 angular.module('mm')
-.controller('DiffTreeController', ["$scope", "$rootScope", "$http", "$state", "$stateParams", "growl", "WorkspaceService",
-function($scope, $rootScope, $http, $state, $stateParams, growl, WorkspaceService) {
-
-    $scope.modelTree = {};
-
-    $scope.modelTree.toggle = {};
-    $scope.modelTree.toggle.mine = false;
-    $scope.modelTree.toggle.theirs = false;
-    $scope.modelTree.toggle.staged = true;
-    $scope.modelTree.toggle.deltas = false;
-
-    $scope.modelTree.data = {};
+.controller('DiffTreeController', ["_", "$scope", "$rootScope", "$http", "$state", "$stateParams", "growl", "WorkspaceService",
+function(_, $scope, $rootScope, $http, $state, $stateParams, growl, WorkspaceService) {
 
     $scope.changes = [];
+
+    $scope.options = {
+      types: {
+        'Element': 'fa fa-square',
+        'Property': 'fa fa-circle',
+        'View': 'fa fa-square',
+        'Dependency': 'fa fa-long-arrow-right',
+        'DirectedRelationship': 'fa fa-long-arrow-right',
+        'Generalization': 'fa fa-chevron-right',
+        'Package': 'fa fa-folder',
+        'Connector': 'fa fa-expand'
+      },
+      statuses: {
+        'moved': { style: "moved", button: 'update' },
+        'added': { style: "addition", button: 'add' },
+        'removed': { style: "removal", button: 'remove' },
+        'updated': { style: "update", button: 'update' },
+        'conflict': "",
+        'resolve': "",
+        'undo': { style: "undo", button: 'undo' }
+      },
+      buttons: {
+        /*"update": {
+          style: "btn btn-primary btn-xs",
+          action: function(branch) { registerChange(branch); } 
+        },
+        "remove": {
+          style: "btn btn-danger btn-xs",
+          action: function(branch) { registerChange(branch); } 
+        },
+        "add": {
+          style: "btn btn-success btn-xs",
+          action: function(branch) { registerChange(branch); } 
+        },
+        "undo": {
+          style: "btn btn-danger btn-xs",
+          action: function(branch) { registerChange(branch); } 
+        }*/
+      } 
+    };
 
     $scope.stageChange = function(change) {
       change.staged = ! change.staged;
     };
 
-    $scope.toggleTree = function(treeType) {
-      $scope.modelTree.toggle.mine = false;
-      $scope.modelTree.toggle.theirs = false;
-      $scope.modelTree.toggle.staged = false;
-      $scope.modelTree.toggle.deltas = false;
-
-      $scope.modelTree.toggle[treeType] = true;
-    };
-
     $scope.selectChange = function (change) {
-      $state.go('main.diff.view', {elementId: change.data.sysmlid});
+      var elementId;
+      if (change.type === "added")
+        elementId = change.delta.sysmlid;
+      else
+        elementId = change.original.sysmlid;
+
+      $state.go('main.diff.view', {elementId: elementId});
     };
 
-    $scope.treeData = [];
-    $scope.id2node = {};
-    $scope.ws1 = {};
-    $scope.ws2id2node = {};
+    $scope.id2change = {};
 
     // Diff the two workspaces picked in the Workspace Picker
     WorkspaceService.diff('ws1', 'ws2').then(
      function(result) {
         
-        $scope.ws1 = result.workspace1;
-        $scope.ws2 = result.workspace2;
-
-        setupModelTree(result.workspace1, result.workspace2);
-
-        $scope.select_branch_id = function (sysmlid) {
-          $state.go('main.diff.view', {elementId: sysmlid});
-        };
+        setupChangesList(result.workspace1, result.workspace2); 
 
       },
       function(reason) {
         growl.error("Workspace diff failed: " + reason.message);
       }
     );   
-
       /*
        * Preps mms-tree with data and display options.
        */
-      var setupModelTree = function(ws1, ws2) {
+      var setupChangesList = function(ws1, ws2) {
 
-        var registerChange = function(branch) {
+        var emptyElement = { name: "", owner: "", documentation: ""};
 
-          if (branch.status === branch.change_type)
-            branch.status = "undo";
-          else
-            branch.status = branch.change_type;
+        var createChange = function (name, element, deltaElement, changeType, changeIcon, treeNode) {
+          var change = {};
+          
+          change.name = name;
+          change.original = element;
+          change.delta = deltaElement;
+          change.type = changeType;
+          change.icon = changeIcon;
+          change.staged = false;
+          change.treeNode = treeNode;
 
-          branch.merge_status = ! branch.merge_status;
+          change.properties = {};
+          change.properties.name = {};
+          change.properties.owner = {};
+          change.properties.documentation = {};
 
-          registerPropertyChange(branch, "name");
-          registerPropertyChange(branch, "owner");
-          registerPropertyChange(branch, "documentation");
+          updateChangeProperty(change.properties.name, "clean");
+          updateChangeProperty(change.properties.owner, "clean");
+          updateChangeProperty(change.properties.documentation, "clean");
+
+          return change;
         };
 
-        var registerPropertyChange = function (branch, property) {
-          if (branch.property_status[property] === branch.property_status_type[property])
-            branch.property_status[property] = "undo";
-          else
-            branch.property_status[property] = branch.property_status_type[property];
-
-          branch.property_merge_status[property] = ! branch.property_merge_status[property];
+        var updateChangeProperty = function(property, changeType) {
+          property.type = changeType;
+          property.staged = false;
         };
 
-        var setBranchState = function (branch, state, status_type, conflict_status, merge_status) {
-          branch.status = state;
-          branch.change_type = status_type;
-          branch.merge_status = false;
+        var id2data = {};
 
-          if (! branch.property_status)
-            branch.property_status = {};
-
-          if (! branch.property_status_type)
-            branch.property_status_type = {};
-
-          if (! branch.property_conflict_status)
-            branch.property_conflict_status = {};
-
-          if (! branch.property_merge_status)
-            branch.property_merge_status = {};
-
-          setPropertyState(branch, "name", state, status_type, conflict_status, merge_status);
-          setPropertyState(branch, "owner", state, status_type, conflict_status, merge_status);
-          setPropertyState(branch, "documentation", state, status_type, conflict_status, merge_status);
-        };
-
-        var setPropertyState = function (branch, property, state, status_type, conflict_status, merge_status) {
-            branch.property_status[property] = state;
-            branch.property_status_type[property]  = status_type;
-            branch.property_conflict_status[property]  = conflict_status;
-            branch.property_merge_status[property]  = merge_status;
-        };
-
-        var id2node = {};
-
-        $scope.id2node = id2node;
-
-        $scope.registerPropertyChange = registerPropertyChange;
-        
-        $scope.options_staged = {
-          types: {
-            'Element': 'fa fa-square',
-            'Property': 'fa fa-circle',
-            'View': 'fa fa-square',
-            'Dependency': 'fa fa-long-arrow-right',
-            'DirectedRelationship': 'fa fa-long-arrow-right',
-            'Generalization': 'fa fa-chevron-right',
-            'Package': 'fa fa-folder',
-            'Connector': 'fa fa-expand'
-          },
-          statuses: {
-            'moved': { style: "update"},
-            'added': { style: "addition"},
-            'removed': { style: "removal"},
-            'updated': { style: "update"},
-            'conflict': "",
-            'resolve': "",
-            'undo': { style: "undo"}
-          }
-        };
-
-        $scope.options = {
-          types: {
-            'Element': 'fa fa-square',
-            'Property': 'fa fa-circle',
-            'View': 'fa fa-square',
-            'Dependency': 'fa fa-long-arrow-right',
-            'DirectedRelationship': 'fa fa-long-arrow-right',
-            'Generalization': 'fa fa-chevron-right',
-            'Package': 'fa fa-folder',
-            'Connector': 'fa fa-expand'
-          },
-          statuses: {
-            'moved': { style: "update", button: 'update' },
-            'added': { style: "addition", button: 'add' },
-            'removed': { style: "removal", button: 'remove' },
-            'updated': { style: "update", button: 'update' },
-            'conflict': "",
-            'resolve': "",
-            'undo': { style: "undo", button: 'undo' }
-          },
-          buttons: {
-            "update": {
-              style: "btn btn-primary btn-xs",
-              action: function(branch) { registerChange(branch); } 
-            },
-            "remove": {
-              style: "btn btn-danger btn-xs",
-              action: function(branch) { registerChange(branch); } 
-            },
-            "add": {
-              style: "btn btn-success btn-xs",
-              action: function(branch) { registerChange(branch); } 
-            },
-            "undo": {
-              style: "btn btn-danger btn-xs",
-              action: function(branch) { registerChange(branch); } 
-            }
-          }
-        };
-
-        // Load up the tree with elements
         ws1.elements.forEach(function(e) {
-          var node = {};
-          node.data = e;
-          node.label = e.name;
-          node.type = e.specialization.type;
-          node.children = [];
-
-          setBranchState(node, "clean", "clean", false, false);
-
-          id2node[e.sysmlid] = node;
+          id2data[e.sysmlid] = e;
         });
 
-        ws1.elements.forEach(function(e) {
-          if (!id2node.hasOwnProperty(e.owner))
-              $scope.treeData.push(id2node[e.sysmlid]);          
-          else
-              id2node[e.owner].children.push(id2node[e.sysmlid]);
-       });
 
         ws2.addedElements.forEach(function(e) {
-          var node = {};
-          node.data = e;
-          node.label = e.name;
-          node.type = e.specialization.type;
-          node.children = [];
+          id2data[e.sysmlid] = e;
 
-          id2node[e.sysmlid] = node;
-          
-          if (e.hasOwnProperty('owner')) {
-            id2node[e.owner].children.push(id2node[e.sysmlid]);
-          } else {
-              $scope.treeData.push(id2node[e.sysmlid]);
-          }
-          
-          setBranchState(node, "added", "added", true, false);
+          var change = createChange(e.name, emptyElement, e, "added", "fa-plus-circle", null);
 
-
-          var change = {};
-          change.data = e;
-          change.type = "added";
-          change.icon = "fa-plus-circle";
-          change.staged = false;
-          change.treeNode = node;
+          updateChangeProperty(change.properties.name, "added");
+          updateChangeProperty(change.properties.owner, "added");
+          updateChangeProperty(change.properties.documentation, "added");
 
           $scope.changes.push(change);
+          $scope.id2change[e.sysmlid] = change;
 
         });
 
         ws2.deletedElements.forEach(function(e) {
-          
-          id2node[e.sysmlid].change_type = "removed";
-          id2node[e.sysmlid].status = "removed";
 
-          setPropertyState(id2node[e.sysmlid], "name", "removed", "removed", true, false);
-          setPropertyState(id2node[e.sysmlid], "owner", "removed", "removed", true, false);
-          setPropertyState(id2node[e.sysmlid], "documentation", "removed", "removed", true, false);
+          var deletedElement = id2data[e.sysmlid];
 
-          var change = {};
-          change.data = e;
-          change.type = "removed";
-          change.icon = "fa-times-circle";
-          change.staged = false;
-          change.treeNode = id2node[e.sysmlid];
+          var change = createChange(deletedElement.name, deletedElement, emptyElement, "removed", "fa-times-circle", null);
+
+          updateChangeProperty(change.properties.name, "removed");
+          updateChangeProperty(change.properties.owner, "removed");
+          updateChangeProperty(change.properties.documentation, "removed");
 
           $scope.changes.push(change);
+          $scope.id2change[e.sysmlid] = change;
 
         });
 
         ws2.updatedElements.forEach(function(e) {
 
-          id2node[e.sysmlid].change_type = "updated";
-          id2node[e.sysmlid].status = "updated";
+          var updatedElement = id2data[e.sysmlid];
 
-          if (e.hasOwnProperty('name'))
-            setPropertyState(id2node[e.sysmlid], "name", "updated", "updated", true, false);
-          if (e.hasOwnProperty('owner'))
-            setPropertyState(id2node[e.sysmlid], "owner", "updated", "updated", true, false);
-          if (e.hasOwnProperty('documentation'))
-            setPropertyState(id2node[e.sysmlid], "documentation", "updated", "updated", true, false);  
+          var deltaElement = _.cloneDeep(updatedElement);
 
-          var change = {};
-          change.data = e;
-          change.type = "updated";
-          change.icon = "fa-pencil";
-          change.staged = false;
-          change.treeNode = id2node[e.sysmlid];
+          var change = createChange(updatedElement.name, updatedElement, deltaElement, "updated", "fa-pencil", null);
+
+          if (e.hasOwnProperty('name')) {
+            change.name = e.name;
+            deltaElement.name = e.name;
+            updateChangeProperty(change.properties.name, "updated");
+          }
+          if (e.hasOwnProperty('owner')) {
+            deltaElement.owner = e.owner;
+            updateChangeProperty(change.properties.owner, "updated");
+          }
+          if (e.hasOwnProperty('documentation')) {
+            deltaElement.documentation = e.documentation;
+            updateChangeProperty(change.properties.documentation, "updated");
+          }
 
           $scope.changes.push(change);
+          $scope.id2change[e.sysmlid] = change;
 
         });
 
         ws2.movedElements.forEach(function(e) {
 
-          id2node[e.sysmlid].change_type = "moved";
-          id2node[e.sysmlid].status = "moved";
+          var movedElement = id2data[e.sysmlid];
 
-          if (e.hasOwnProperty('name'))
-            setPropertyState(id2node[e.sysmlid], "name", "moved", "moved", true, false);
-          if (e.hasOwnProperty('owner'))
-            setPropertyState(id2node[e.sysmlid], "owner", "moved", "moved", true, false);
-          if (e.hasOwnProperty('documentation'))
-            setPropertyState(id2node[e.sysmlid], "documentation", "moved", "moved", true, false);
+          var deltaElement = _.cloneDeep(movedElement);
 
-          var change = {};
-          change.data = e;
-          change.type = "moved";
-          change.icon = "fa-arrows";
-          change.staged = false;
-          change.treeNode = id2node[e.sysmlid];
+          var change = createChange(movedElement.name, movedElement, deltaElement, "moved", "fa-arrows", null);
+
+          if (e.hasOwnProperty('owner')) {
+            deltaElement.owner = e.owner;
+            updateChangeProperty(change.properties.owner, "moved");
+          }
 
           $scope.changes.push(change);
+          $scope.id2change[e.sysmlid] = change;
 
         });
       };
+
 }])
 .controller('DiffViewController', ["$scope", "$rootScope", "$http", "$state", "$stateParams", "_", "growl", "WorkspaceService",
 function($scope, $rootScope, $http, $state, $stateParams, _, growl, WorkspaceService) {
  
     var elementId = $stateParams.elementId;
 
-    var originalNode = $scope.id2node[elementId];
-
-    $scope.original = originalNode.data;
-    $scope.originalNode = originalNode;
-
-    // make the delta object
-    var delta = _.cloneDeep(originalNode.data);
-
-    if (originalNode.change_type === "updated") {
-
-    }
-
-    $scope.delta = delta;
-
+    $scope.change = $scope.id2change[elementId];
+    
 }]);
