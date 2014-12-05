6d83095d0d4348380313f819c8cf1cc3131f0d5c
Raffi P Tikidjian <raffi@jpl.nasa.gov>
2014-12-05 14:09:11 -0800

    add button bar directive, refactor button bar in model manager

--- src/directives/mmsButtonBar.js
+++ src/directives/mmsButtonBar.js
@@ -7,13 +7,80 @@ function mmsButtonBar($templateCache) {
     var template = $templateCache.get('mms/templates/mmsButtonBar.html');
 
     var mmsButtonBarLink = function(scope, element, attrs){
+        scope.clicked = function(button) {
+            if (! button.active)
+                return;
 
+            if (button.onClick)
+                button.onClick();
+            else if (scope.onClick)
+                scope.onClick({button: button});
+
+            scope.buttons.forEach(function(b) {
+                if (b === button) {
+                    b.selected = true;
+                } else
+                    b.selected = false;
+            });
+        };
+    };
+
+    var mmsButtonBarCtrl = function($scope) {
+        if ($scope.mmsBbApi) {
+            var api = $scope.mmsBbApi;
+
+            api.select = function(id) {
+                $scope.buttons.forEach(function(button) {
+                    if (button.id === id && button.active) {
+                        button.selected = true;
+                        $scope.clicked(button);
+                    }
+                    else
+                        button.selected = false;
+                });
+            };
+
+            api.setPermission = function (id, permission) {
+                $scope.buttons.forEach(function(button) {
+                    if (button.id === id)
+                        button.permission = permission;
+                });
+            };
+
+            api.addButton = function(button) {
+                
+                if ($scope.buttons.count === 0) {
+                    button.placement = "bottom-left";
+                }
+                else {
+                    button.placement = "bottom";
+                }
+
+                $scope.buttons.push(button);
+            };
+
+            api.toggleButtonSpinner = function (id) {
+                $scope.buttons.forEach(function(button) {
+                    if (button.id === id) {
+                        if (button.spinner) {
+                            button.icon = button.icon_original;
+                        }
+                        else {
+                            button.icon_original = button.icon;
+                            button.icon = 'fa fa-spinner fa-spin';
+                        }
+                        button.spinner = ! button.spinner;
+                    }
+                });
+            };
+        }
     };
 
     return {
         restrict: 'E', 
         template: template,
         link: mmsButtonBarLink,
+        controller: ['$scope', mmsButtonBarCtrl],
         scope: {
             buttons: '=',
             mmsBbApi: '=',
--- src/services/UxService.js
+++ src/services/UxService.js
@@ -26,42 +26,69 @@ function UxService($rootScope) {
     var getToolbarButton = function(button) {
 		switch (button) {
 		  case "element.viewer":
-		  	return {id: 'element.viewer', icon: 'fa fa-eye', selected: true, active: true, permission:true, tooltip: 'Preview Element', 
-            		spinner: false, onClick: function() {$rootScope.$broadcast('element.viewer');}};
+		  	return {id: button, icon: 'fa fa-eye', selected: true, active: true, permission:true, tooltip: 'Preview Element', 
+            		spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		  case "element.editor":
-		    return {id: 'element.editor', icon: 'fa fa-edit', selected: false, active: true, permission:false, tooltip: 'Edit Element',
-		            spinner: false, onClick: function() {$rootScope.$broadcast('element.editor');},
+		    return {id: button, icon: 'fa fa-edit', selected: false, active: true, permission:false, tooltip: 'Edit Element',
+		            spinner: false, onClick: function() {$rootScope.$broadcast(button);},
 		        	dynamic_buttons: [getToolbarButton("element.editor.save"), getToolbarButton("element.editor.cancel")]};
 		  case "view.reorder":
-	        return {id: 'view.reorder', icon: 'fa fa-arrows-v', selected: false, active: true, permission:false, tooltip: 'Reorder View',
-	    	        spinner: false, onClick: function() {$rootScope.$broadcast('view.reorder');},
+	        return {id: button, icon: 'fa fa-arrows-v', selected: false, active: true, permission:false, tooltip: 'Reorder View',
+	    	        spinner: false, onClick: function() {$rootScope.$broadcast(button);},
 		        	dynamic_buttons: [getToolbarButton("view.reorder.save"), getToolbarButton("view.reorder.cancel")]};
 		  case "document.snapshot":
-		  	return  {id: 'document.snapshot', icon: 'fa fa-camera', selected: false, active: true, permission:true, tooltip: 'Snapshots',
-		            spinner: false, onClick: function() {$rootScope.$broadcast('document.snapshot');},
+		  	return  {id: button, icon: 'fa fa-camera', selected: false, active: true, permission:true, tooltip: 'Snapshots',
+		            spinner: false, onClick: function() {$rootScope.$broadcast(button);},
 		        	dynamic_buttons: [getToolbarButton("document.snapshot.refresh"), getToolbarButton("document.snapshot.create")]};
 		  case "element.editor.save":
-			return {id: 'element.editor.save', icon: 'fa fa-save', pullDown: true, dynamic: true, selected: false, active: false, permission:true, tooltip: 'Save',
-				        spinner: false, onClick: function() {$rootScope.$broadcast('element.editor.save');}};
+			return {id: button, icon: 'fa fa-save', pullDown: true, dynamic: true, selected: false, active: false, permission:true, tooltip: 'Save',
+				        spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		  case "element.editor.cancel":
-			return {id: 'element.editor.cancel', icon: 'fa fa-times', dynamic: true, selected: false, active: false, permission:true, tooltip: 'Cancel',
-			            spinner: false, onClick: function() {$rootScope.$broadcast('element.editor.cancel');}};
+			return {id: button, icon: 'fa fa-times', dynamic: true, selected: false, active: false, permission:true, tooltip: 'Cancel',
+			            spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		  case "view.reorder.save":
-			return {id: 'view.reorder.save', icon: 'fa fa-save', pullDown: true, dynamic: true, selected: false, active: false, permission:true, tooltip: 'Save',
-			            spinner: false, onClick: function() {$rootScope.$broadcast('view.reorder.save');}};
+			return {id: button, icon: 'fa fa-save', pullDown: true, dynamic: true, selected: false, active: false, permission:true, tooltip: 'Save',
+			            spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		  case "view.reorder.cancel":
-		    return {id: 'view.reorder.cancel', icon: 'fa fa-times', dynamic: true, selected: false, active: false, permission:true, tooltip: 'Cancel',
-		            spinner: false, onClick: function() {$rootScope.$broadcast('view.reorder.cancel');}};
+		    return {id: button, icon: 'fa fa-times', dynamic: true, selected: false, active: false, permission:true, tooltip: 'Cancel',
+		            spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		  case "document.snapshot.refresh":
-		    return {id: 'document.snapshot.refresh', icon: 'fa fa-refresh', pullDown: true, dynamic: true, selected: false, active: false, permission:true, tooltip: 'Refresh',
-		            spinner: false, onClick: function() {$rootScope.$broadcast('document.snapshot.refresh');}};
+		    return {id: button, icon: 'fa fa-refresh', pullDown: true, dynamic: true, selected: false, active: false, permission:true, tooltip: 'Refresh',
+		            spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		  case "document.snapshot.create":
-		    return {id: 'document.snapshot.create', icon: 'fa fa-plus', dynamic: true, selected: false, active: false, permission:false, tooltip: 'Create Snapshot',
-		            spinner: false, onClick: function() {$rootScope.$broadcast('document.snapshot.create');}};
+		    return {id: button, icon: 'fa fa-plus', dynamic: true, selected: false, active: false, permission:false, tooltip: 'Create Snapshot',
+		            spinner: false, onClick: function() {$rootScope.$broadcast(button);}};
 		}    
 	};
 
+	var getButtonBarButton = function(button) {
+		switch (button) {
+		  case "tree.expand":
+		  	return {id: button, icon: 'fa-caret-square-o-down', selected: true, active: true, permission: true, tooltip: 'Expand All', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+		  case "tree.collapse":
+		  	return {id: button, icon: 'fa-caret-square-o-up', selected: true, active: true, permission: true, tooltip: 'Collapse All', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+		  case "tree.filter":
+		  	return {id: button, icon: 'fa-filter', selected: true, active: true, permission: true, tooltip: 'Filter', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+		  case "tree.add.task":
+		  	return {id: button, icon: 'fa-plus', selected: true, active: true, permission: true, tooltip: 'Add Task', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+		  case "tree.add.configuration":
+		  	return {id: button, icon: 'fa-tag', selected: true, active: true, permission: true, tooltip: 'Add Configuration', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+		  case "tree.delete":
+		  	return {id: button, icon: 'fa-times', selected: true, active: true, permission: true, tooltip: 'Delete', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+		  case "tree.merge":
+		  	return {id: button, icon: 'fa-share-alt fa-flip-horizontal', selected: true, active: true, permission: true, tooltip: 'Merge Task', 
+            		spinner: false, togglable: false, action: function() {$rootScope.$broadcast(button);}};
+        }
+	};
+
     return {
+    	getButtonBarButton: getButtonBarButton,
         getToolbarButton: getToolbarButton
     };
 }
