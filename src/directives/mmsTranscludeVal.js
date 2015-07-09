4135cf3f929de07a9880d53eecac112d418faec4
Erik <Dan.e.karlsson@jpl.nasa.gov>
2015-07-09 15:31:30 -0700

    Works on brad's server using sysmlid - 302 for expression, constraint_32165, and plusExpr

--- src/directives/mmsTranscludeVal.js
+++ src/directives/mmsTranscludeVal.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('mms.directives')
-    .directive('mmsTranscludeVal', ['ElementService', 'UtilsService', 'UxService', 'Utils', 'URLService', '$http', '_', '$log', '$compile', '$templateCache', 'growl', mmsTranscludeVal]);
+angular.module('mms.directives').directive('mmsTranscludeVal', ['ElementService', 'UtilsService', 'UxService', 'Utils', 'URLService', '$http', '_', '$log', '$compile', '$templateCache', 'growl', mmsTranscludeVal]);
 
 /**
  * @ngdoc directive
@@ -53,17 +52,19 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
         var processed = false;
         scope.cfType = 'val';
         element.click(function (e) {
-            if (scope.addFrame)
-                scope.addFrame();
+                if (scope.addFrame) {
+                    scope.addFrame();
+                }
 
-            if (mmsViewCtrl)
-                mmsViewCtrl.transcludeClicked(scope.mmsEid);
+                if (mmsViewCtrl) {
+                    mmsViewCtrl.transcludeClicked(scope.mmsEid);
+                }
 
-            /*if (e.target.tagName !== 'A' && e.target.tagName !== 'INPUT' && !scope.isEditing) //need review for inline editing (boolean and nested)
-             return false;
-             if (scope.isEditing)*/
-            e.stopPropagation();
-        });
+                /*if (e.target.tagName !== 'A' && e.target.tagName !== 'INPUT' && !scope.isEditing) //need review for inline editing (boolean and nested)
+                 return false;
+                 if (scope.isEditing)*/
+                e.stopPropagation();
+            });
 
         var recompile = function () {
             if (scope.recompileScope) {
@@ -76,69 +77,72 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
             var isProperty = false;
             var isConstraint = false;
 
-            for (var i = 0; i < scope.values.length; i++) {
-                if (scope.values[i].type === 'LiteralString') {
+            for (var i = 0; i <
+            scope.values.length; i++)
+            {
+                if (scope.values[i].type ===
+                    'LiteralString')
+                {
                     areStrings = true;
                     var s = scope.values[i].string;
-                    if (s.indexOf('<p>') === -1) {
+                    if (s.indexOf('<p>') ===
+                        -1)
+                    {
                         s = s.replace('<', '&lt;');
                     }
                     toCompileList.push(s);
-                }
-                else if (scope.values[i].type === "Expression") {
+                    console.log("Lit string");
+                } else if (scope.values[i].type ===
+                    "Expression")
+                {
                     isExpression = true;
+                    console.log("Tis an expressions");
                     break;
-                }
-                else if (scope.values[i].type === "Property" && scope.values[i].value.type === "Expression") {
-                    isProperty = true;
-                    break;
-                }
-                else if (scope.values[i].type === "Constraint") {
-                    isConstraint = true;
-                    break;
-                }
-                else {
+                } else {
                     break;
                 }
             }
 
-            element.empty();
-            scope.recompileScope = scope.$new();
-
             // Checks if element is an expression
-            if (isExpression || isConstraint || isProperty) {
+            if (isExpression /*|| isConstraint || isProperty*/) {
                 // Added in response statement when value.type is
                 // Expression
-                $http.get(URLService.getElementURL(scope.mmsEid, scope.ws, scope.version) + '?evaulate')
-                    .success(function (data, status, headers, config) {
+                $http.get(URLService.getElementURL(scope.mmsEid, scope.ws, scope.version) +
+                    '?evaluate').success(function (data, status, headers, config) {
                         if (isExpression) {
-                            element.html(data.elements[0].specialization.evaluation);
-                        } else if (isConstraint) {
-                            element.html(data.elements[0].specialization.specification.operand);
-                        } else {
-                            element.html(data.elements[0].specialization.value.operand);
+                            element.html(data.elements[0].evaluationResult);
                         }
-                        // TODO : change evaluation to evaluationResult
                     }).error(function (data, status, headers, config) {
                         //URLService.handleHttpStatus(data, status, headers, config, deferred);
                         //TODO: Needs case statement when .error is thrown
                     });
                 return;
-            }
-            else {
-                if (scope.values.length === 0 || Object.keys(scope.values[0]).length < 2) {
-                    element.html('<span>' + ((scope.version === 'latest') ? '(no value)' : '') + '</span>');
+            } else {
+                element.empty();
+                scope.recompileScope = scope.$new();
+                if (scope.values.length ===
+                    0 ||
+                    Object.keys(scope.values[0]).length <
+                    2)
+                {
+                    element.html('<span>' +
+                        ((scope.version ===
+                        'latest') ? '(no value)' : '') +
+                        '</span>');
                 } else if (areStrings) {
                     var toCompile = toCompileList.join(' ');
-                    if (toCompile === '') {
-                        element.html('<span>' + ((scope.version === 'latest') ? '(no value)' : '') + '</span>');
+                    if (toCompile === '')
+                    {
+                        element.html('<span>' +
+                            ((scope.version ===
+                            'latest') ? '(no value)' : '') +
+                            '</span>');
                         return;
                     }
                     element.append(toCompile);
                     $compile(element.contents())(scope.recompileScope);
                 } else if (UtilsService.isRestrictedValue(scope.values)) {
-                    ElementService.getElement(scope.values[0].operand[1].element, false, scope.ws, scope.version)
-                        .then(function (e) {
+                    ElementService.getElement(scope.values[0].operand[1].element, false, scope.ws, scope.version).then(function (e) {
                             scope.isRestrictedVal = true;
                             element.html(e.name);
                         });
@@ -153,15 +157,22 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
         };
 
         var recompileEdit = function () {
-            if (scope.recompileScope)
+            if (scope.recompileScope) {
                 scope.recompileScope.$destroy();
+            }
             var toCompileList = [];
             var areStrings = false;
-            for (var i = 0; i < scope.editValues.length; i++) {
-                if (scope.editValues[i].type === 'LiteralString') {
+            for (var i = 0; i <
+            scope.editValues.length; i++)
+            {
+                if (scope.editValues[i].type ===
+                    'LiteralString')
+                {
                     areStrings = true;
                     var s = scope.editValues[i].string;
-                    if (s.indexOf('<p>') === -1) {
+                    if (s.indexOf('<p>') ===
+                        -1)
+                    {
                         s = s.replace('<', '&lt;');
                     }
                     toCompileList.push(s);
@@ -171,19 +182,32 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
             }
             element.empty();
             scope.recompileScope = scope.$new();
-            if (scope.editValues.length === 0 || Object.keys(scope.editValues[0]).length < 2)
-                element.html('<span' + ((scope.version === 'latest') ? '' : ' class="placeholder"') + '>(no value)</span>');
-            else if (areStrings) {
+            if (scope.editValues.length ===
+                0 ||
+                Object.keys(scope.editValues[0]).length <
+                2)
+            {
+                element.html('<span' +
+                    ((scope.version ===
+                    'latest') ? '' : ' class="placeholder"') +
+                    '>(no value)</span>');
+            } else if (areStrings) {
                 var toCompile = toCompileList.join(' ');
-                if (toCompile === '') {
-                    element.html('<span' + ((scope.version === 'latest') ? '' : ' class="placeholder"') + '>(no value)</span>');
+                if (toCompile ===
+                    '')
+                {
+                    element.html('<span' +
+                        ((scope.version ===
+                        'latest') ? '' : ' class="placeholder"') +
+                        '>(no value)</span>');
                     return;
                 }
-                element.append('<div class="panel panel-info">' + toCompile + '</div>');
+                element.append('<div class="panel panel-info">' +
+                    toCompile +
+                    '</div>');
                 $compile(element.contents())(scope.recompileScope);
             } else if (UtilsService.isRestrictedValue(scope.editValues)) {
-                ElementService.getElement(scope.editValues[0].operand[1].element, false, scope.ws, scope.version)
-                    .then(function (e) {
+                ElementService.getElement(scope.editValues[0].operand[1].element, false, scope.ws, scope.version).then(function (e) {
                         element.html(e.name);
                     });
             } else {
@@ -193,67 +217,106 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
         };
 
         var idwatch = scope.$watch('mmsEid', function (newVal, oldVal) {
-            if (!newVal)
-                return;
-            idwatch();
-            if (UtilsService.hasCircularReference(scope, scope.mmsEid, 'val')) {
-                //$log.log("prevent circular dereference!");
-                element.html('<span class="error">Circular Reference!</span>');
-                return;
-            }
-            var ws = scope.mmsWs;
-            var version = scope.mmsVersion;
-            if (mmsViewCtrl) {
-                var viewVersion = mmsViewCtrl.getWsAndVersion();
-                if (!ws)
-                    ws = viewVersion.workspace;
-                if (!version)
-                    version = viewVersion.version;
-            }
-            scope.ws = ws;
-            scope.version = version ? version : 'latest';
-            ElementService.getElement(scope.mmsEid, false, ws, version)
-                .then(function (data) {
-                    scope.element = data;
-                    scope.values = scope.element.specialization.value;
-                    if (scope.element.specialization.type === 'Constraint' && scope.element.specialization.specification)
-                        scope.values = [scope.element.specialization.specification];
-                    recompile();
-                    //scope.$watch('values', recompile, true);
-                    if (scope.version === 'latest') {
-                        scope.$on('element.updated', function (event, eid, ws, type) {
-                            if (eid === scope.mmsEid && ws === scope.ws && (type === 'all' || type === 'value'))
-                                recompile();
-                        });
+                if (!newVal) {
+                    return;
+                }
+                idwatch();
+                if (UtilsService.hasCircularReference(scope, scope.mmsEid, 'val')) {
+                    //$log.log("prevent circular dereference!");
+                    element.html('<span class="error">Circular Reference!</span>');
+                    return;
+                }
+                var ws = scope.mmsWs;
+                var version = scope.mmsVersion;
+                if (mmsViewCtrl) {
+                    var viewVersion = mmsViewCtrl.getWsAndVersion();
+                    if (!ws) {
+                        ws = viewVersion.workspace;
                     }
-                }, function (reason) {
-                    var status = ' not found';
-                    if (reason.status === 410)
-                        status = ' deleted';
-                    element.html('<span class="error">value cf ' + newVal + status + '</span>');
-                    growl.error('Cf Val Error: ' + reason.message + ': ' + scope.mmsEid);
-                });
-        });
+                    if (!version) {
+                        version = viewVersion.version;
+                    }
+                }
+                scope.ws = ws;
+                scope.version = version ? version : 'latest';
+                ElementService.getElement(scope.mmsEid, false, ws, version).then(function (data) {
+                        scope.element = data;
+                        scope.values = scope.element.specialization.value;
+                        if (scope.element.specialization.type ===
+                            'Constraint' &&
+                            scope.element.specialization.specification)
+                        {
+                            scope.values = [scope.element.specialization.specification];
+                        }
+                        if (scope.element.specialization.type ===
+                            'Expression')
+                        {
+                            scope.values = [scope.element.specialization];//TODO : Assigned spec type :
+                            // expression
+                            // into it's own value
+                        }
+                        recompile();
+                        //scope.$watch('values', recompile, true);
+                        if (scope.version ===
+                            'latest')
+                        {
+                            scope.$on('element.updated', function (event, eid, ws, type) {
+                                    if (eid ===
+                                        scope.mmsEid &&
+                                        ws ===
+                                        scope.ws &&
+                                        (type ===
+                                        'all' ||
+                                        type ===
+                                        'value'))
+                                    {
+                                        recompile();
+                                    }
+                                });
+                        }
+                    }, function (reason) {
+                        var status = ' not found';
+                        if (reason.status ===
+                            410)
+                        {
+                            status = ' deleted';
+                        }
+                        element.html('<span class="error">value cf ' +
+                            newVal +
+                            status +
+                            '</span>');
+                        growl.error('Cf Val Error: ' +
+                            reason.message +
+                            ': ' +
+                            scope.mmsEid);
+                    });
+            });
 
         scope.hasHtml = function (s) {
             return Utils.hasHtml(s);
         };
 
         scope.addValueTypes = {
-            string: 'LiteralString',
-            boolean: 'LiteralBoolean',
-            integer: 'LiteralInteger',
-            real: 'LiteralReal'
+            string: 'LiteralString', boolean: 'LiteralBoolean', integer: 'LiteralInteger', real: 'LiteralReal'
         };
         scope.addValue = function (type) {
-            if (type === 'LiteralBoolean')
+            if (type ===
+                'LiteralBoolean')
+            {
                 scope.editValues.push({type: type, boolean: false});
-            else if (type === 'LiteralInteger')
+            } else if (type ===
+                'LiteralInteger')
+            {
                 scope.editValues.push({type: type, integer: 0});
-            else if (type === 'LiteralString')
+            } else if (type ===
+                'LiteralString')
+            {
                 scope.editValues.push({type: type, string: ''});
-            else if (type === 'LiteralReal')
+            } else if (type ===
+                'LiteralReal')
+            {
                 scope.editValues.push({type: type, double: 0.0});
+            }
         };
         scope.addValueType = 'LiteralString';
 
@@ -272,8 +335,8 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
             mmsViewCtrl.registerPresenElemCallBack(callback);
 
             scope.$on('$destroy', function () {
-                mmsViewCtrl.unRegisterPresenElemCallBack(callback);
-            });
+                    mmsViewCtrl.unRegisterPresenElemCallBack(callback);
+                });
 
             scope.save = function () {
                 Utils.saveAction(scope, recompile, scope.bbApi, null, type, element);
@@ -287,10 +350,9 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
                 if (scope.isRestrictedVal) {
                     var options = [];
                     scope.values[0].operand[2].operand.forEach(function (o) {
-                        options.push(o.element);
-                    });
-                    ElementService.getElements(options, false, scope.ws, scope.version)
-                        .then(function (elements) {
+                            options.push(o.element);
+                        });
+                    ElementService.getElements(options, false, scope.ws, scope.version).then(function (elements) {
                             scope.options = elements;
                             Utils.addFrame(scope, mmsViewCtrl, element, frameTemplate);
                         });
@@ -312,16 +374,12 @@ function mmsTranscludeVal(ElementService, UtilsService, UxService, Utils, URLSer
             scope.instanceVal = mmsViewPresentationElemCtrl.getInstanceVal();
             scope.presentationElem = mmsViewPresentationElemCtrl.getPresentationElement();
         }
-    }
-        ;
+    };
 
     return {
-        restrict: 'E',
-        //template: template,
+        restrict: 'E', //template: template,
         scope: {
-            mmsEid: '@',
-            mmsWs: '@',
-            mmsVersion: '@'
+            mmsEid: '@', mmsWs: '@', mmsVersion: '@'
         },
         require: ['?^mmsView', '?^mmsViewPresentationElem'],
         controller: ['$scope', mmsTranscludeCtrl],
