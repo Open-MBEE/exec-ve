aeaed2e15dafc66b6e3dde8be5443fd34dba27e1
Piette, Marie Y (172L) <Marie.Y.Piette@jpl.nasa.gov>
2015-07-17 09:22:04 -0700

    Very basics of skin cover

--- Gruntfile.js
+++ Gruntfile.js
@@ -422,7 +422,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: 'gov.nasa.jpl:evm:zip',
-            version: '0.2.2-SNAPSHOT',
+            version: '0.2.2.arrm-SNAPSHOT',
             path: 'deploy/'
           }]
         }
