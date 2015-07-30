f20e8043a4c1dad18bb7b713282108126b99bb76
Piette, Marie Y (172L) <Marie.Y.Piette@jpl.nasa.gov>
2015-07-30 16:52:23 -0700

    Hopefully finished static html portal page

--- app/js/mms/app.js
+++ app/js/mms/app.js
@@ -154,18 +154,18 @@ angular.module('mmsApp', ['mms', 'mms.directives', 'fa.directive.borderLayout', 
             } 
         }        
     })
-    .state('workspace.arrmportal', {
-        url: '/arrmportal',
-        resolve: {
-        },
-        parent: 'workspace',
-        views: {
-            'pane-center@': {
-                templateUrl: 'partials/testerrr/partialsArrmHomePresent.html',
-                controller: 'slideShowCtrl'
-            }
-        }
-    })
+    // .state('workspace.arrmportal', {
+    //     url: '/arrmportal',
+    //     resolve: {
+    //     },
+    //     parent: 'workspace',
+    //     views: {
+    //         'pane-center@': {
+    //             templateUrl: 'partials/testerrr/partialsArrmHomePresent.html',
+    //             controller: 'slideShowCtrl'
+    //         }
+    //     }
+    // })
 
     .state('workspace', {
         parent: 'workspaces',
