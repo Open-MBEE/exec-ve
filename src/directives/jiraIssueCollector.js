c6d0bacbf49280ea86162ec2697f10af39853c5e
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2015-09-15 16:39:06 -0700

    add id and text option

--- src/directives/jiraIssueCollector.js
+++ src/directives/jiraIssueCollector.js
@@ -5,39 +5,37 @@ angular.module('mms.directives')
 
 /**
  * @ngdoc directive
- * @name mms.directives.directive:mmsTranscludeName
+ * @name mms.directives.directive:jiraIssueCollector
  *
- * @requires mms.ElementService
  * @requires $compile
  *
  * @restrict E
  *
  * @description
- * Given an element id, puts in the element's name binding, if there's a parent 
- * mmsView directive, will notify parent view of transclusion on init and name change,
- * and on click
+ * Given a collector id and text, puts in a button with text that brings up jira issue collector
+ * (on ehm jira)
  *
- * @param {string} mmsEid The id of the element whose name to transclude
- * @param {string=master} mmsWs Workspace to use, defaults to master
- * @param {string=latest} mmsVersion Version can be alfresco version number or timestamp, default is latest
+ * @param {string} collectorId The id of the jira issue collector
+ * @param {string} text button text
  */
 function jiraIssueCollector($templateCache, $compile) {
     var jiraIssueCollectorLink = function(scope, element, attrs) {
         //element.append($templateCache.get('mms/templates/jiraIssueCollector.html'));
         //$compile(element.contents())(scope); 
-        window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, {
-                    'ae470811' : {
-                        triggerFunction : function( showCollectorDialog ) {
-                            $('.ae470811').click( function(e) {
-                                e.preventDefault();
-                                e.stopPropagation();
-                                showCollectorDialog();
-                            });
-                        }
-                    }
+        var id = scope.collectorId;
+        var extendOb = {};
+        extendOb[id] = {
+            triggerFunction : function( showCollectorDialog ) {
+                $('.' + id).click( function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    showCollectorDialog();
                 });
+            }
+        };
+        window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, extendOb);
         jQuery.ajax({
-            url: "https://ehm.jpl.nasa.gov/jira/s/d41d8cd98f00b204e9800998ecf8427e/en_US-1rh5yq-1988229788/6163/44/1.4.3/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=ae470811",
+            url: 'https://ehm.jpl.nasa.gov/jira/s/d41d8cd98f00b204e9800998ecf8427e/en_US-1rh5yq-1988229788/6163/44/1.4.3/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=' + id,
             type: "get",
             cache: false,
             dataType: "script"
@@ -46,7 +44,11 @@ function jiraIssueCollector($templateCache, $compile) {
 
     return {
         restrict: 'E',
-        template: '<button class="ae470811">bam</button>',
+        scope: {
+            collectorId: '@',
+            text: '@'
+        },
+        template: '<button class="{{collectorId}}">{{text}}</button>',
         link: jiraIssueCollectorLink
     };
 }
