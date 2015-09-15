27ecdc4e338bf6be0f8eb701739e7f11de4902a8
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2015-09-15 16:24:42 -0700

    omg typo

--- src/directives/jiraIssueCollector.js
+++ src/directives/jiraIssueCollector.js
@@ -23,29 +23,30 @@ angular.module('mms.directives')
  */
 function jiraIssueCollector($templateCache, $compile) {
     var jiraIssueCollectorLink = function(scope, element, attrs) {
-        element.append($templateCache.get('mms/templates/jiraIssueCollector.html'));
-        $compile(element.contents())(scope); 
-        /*window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, {
-                    //'ae47081' : {
+        //element.append($templateCache.get('mms/templates/jiraIssueCollector.html'));
+        //$compile(element.contents())(scope); 
+        window.ATL_JQ_PAGE_PROPS = $.extend(window.ATL_JQ_PAGE_PROPS, {
+                    'ae470811' : {
                         triggerFunction : function( showCollectorDialog ) {
-                            $('.ae47081').click( function(e) {
+                            $('.ae470811').click( function(e) {
                                 e.preventDefault();
+                                e.stopPropagation();
                                 showCollectorDialog();
                             });
                         }
-                    //}
+                    }
                 });
         jQuery.ajax({
-            url: "https://ehm.jpl.nasa.gov/jira/s/d41d8cd98f00b204e9800998ecf8427e/en_US-1rh5yq-1988229788/6163/44/1.4.3/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?collectorId=ae47081",
+            url: "https://ehm.jpl.nasa.gov/jira/s/d41d8cd98f00b204e9800998ecf8427e/en_US-1rh5yq-1988229788/6163/44/1.4.3/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=ae470811",
             type: "get",
             cache: false,
             dataType: "script"
-        });*/
+        });
     };
 
     return {
         restrict: 'E',
-        //template: '<button class="ae47081">bam</button>',
+        template: '<button class="ae470811">bam</button>',
         link: jiraIssueCollectorLink
     };
 }
