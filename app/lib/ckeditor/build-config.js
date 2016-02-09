b746233e903d4971691569e17000101d54967d6a
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2016-02-09 15:59:32 -0800

    update ckeditor version and plugins. remove obsolete plugins

--- app/lib/ckeditor/build-config.js
+++ app/lib/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/67a15e8f7aab30b456b3a86335d11690
+ * (2) http://ckeditor.com/builder/c3348cbea3a7110a4a9b482aac425464
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/67a15e8f7aab30b456b3a86335d11690
+ * (3) http://ckeditor.com/builder/download/c3348cbea3a7110a4a9b482aac425464
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -52,10 +52,10 @@ var CKBUILDER_CONFIG = {
 	plugins : {
 		'a11yhelp' : 1,
 		'about' : 1,
+		'autoembed' : 1,
 		'autogrow' : 1,
 		'base64image' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
 		'colorbutton' : 1,
@@ -64,8 +64,10 @@ var CKBUILDER_CONFIG = {
 		'dialogadvtab' : 1,
 		'div' : 1,
 		'elementspath' : 1,
+		'embed' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'eqneditor' : 1,
 		'filebrowser' : 1,
 		'find' : 1,
 		'flash' : 1,
@@ -80,37 +82,32 @@ var CKBUILDER_CONFIG = {
 		'indentblock' : 1,
 		'indentlist' : 1,
 		'justify' : 1,
-		'language' : 1,
 		'link' : 1,
 		'list' : 1,
 		'liststyle' : 1,
 		'magicline' : 1,
 		'mathjax' : 1,
 		'maximize' : 1,
-		'newpage' : 1,
+		'mediaembed' : 1,
 		'pagebreak' : 1,
-		'pastefromword' : 1,
+		'pastebase64' : 1,
 		'pastetext' : 1,
-		'preview' : 1,
-		'print' : 1,
+		'pbckcode' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
 		'save' : 1,
-		'scayt' : 1,
 		'selectall' : 1,
 		'showblocks' : 1,
 		'showborders' : 1,
-		'smiley' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
+		'symbol' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
-		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
