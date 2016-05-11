f45cfde98b6127882e58e4bc037ba375adfb6044
Laura Mann <Laura.Mann@jpl.nasa.gov>
2016-05-11 14:02:49 -0700

    Changed server settings for host name from test state.

--- src/services/StompService.js
+++ src/services/StompService.js
@@ -14,13 +14,12 @@ angular.module('mms')
 function StompService($rootScope, UtilsService, $window, $location, ApplicationService, CacheService) {
      var stompClient = {};
      var host = $location.host();
-    //  var hostName = 'wss://'+$location.host().split(".")[0]+'-origin.jpl.nasa.gov:61614'; 
-    //  if (host == '127.0.0.1') {
-    //     hostName = 'wss://127.0.0.1:61614'; 
-    //  } else if (host == 'localhost') {
-    //     hostName = 'wss://localhost:61614';
-    //  }
-    var hostName = 'wss://cae-ems-uat-origin.jpl.nasa.gov:61614'; 
+     var hostName = 'wss://'+$location.host().split(".")[0]+'-origin.jpl.nasa.gov:61614'; 
+     if (host == '127.0.0.1') {
+        hostName = 'wss://127.0.0.1:61614'; 
+     } else if (host == 'localhost') {
+         hostName = 'wss://localhost:61614';
+     }
     
     var stompSuccessCallback = function(message){
         var updateWebpage = angular.fromJson(message.body);
