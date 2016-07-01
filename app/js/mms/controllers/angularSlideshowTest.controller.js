5c81c28577ee99984509c63e19a796b510cd0360
Tikidjian, Raffi P (393A) <Raffi.P.Tikidjian@jpl.nasa.gov>
2016-07-01 08:21:10 -0700

    remove more unused portal files

--- app/js/mms/controllers/angularSlideshowTest.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('slideShowExample', ['simple-slideshow'])
-    .controller('slideShowCtrl', ['$scope', function($scope){
-        $scope.slides = [
-            {name: 'Not my cat.', url: 'https://farm2.staticflickr.com/1318/5114665665_e55b2c2169_n.jpg'},
-            {name: 'Again, not my cat.', url: 'https://farm2.staticflickr.com/1079/901626554_8bc51ec160_n.jpg'}];
-    }]);
