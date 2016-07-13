f78c330c67f6bbb842b52325e15b7dd2d246dcb9
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2016-07-13 10:13:13 -0700

    VE-47 - create new widget to allow for dropping images into editor. New widget uses upload widget

---
+++ app/lib/ckeditor/plugins/imagedrop/plugin.js
@@ -0,0 +1,130 @@
+/**
+ * imagedrop plugin for CKEDITOR
+ * @author Shakeh Khudikyan
+ * This plugin requires uploadwidget to 
+ */
+
+'use strict';
+
+( function() {
+	CKEDITOR.plugins.add( 'imagedrop', {
+		requires: 'uploadwidget',
+
+		onLoad: function() {
+			CKEDITOR.addCss(
+				'.cke_upload_uploading img{' +
+					'opacity: 0.3' +
+				'}'
+			);
+		},
+
+		init: function( editor ) {
+			// Do not execute this paste listener if it will not be possible to upload file.
+			if ( !CKEDITOR.plugins.clipboard.isFileApiSupported ) {
+				return;
+			}
+
+			var fileTools = CKEDITOR.fileTools;
+
+			// Handle images which are available in the dataTransfer.
+			fileTools.addUploadWidget( editor, 'imagedrop', {
+				supportedTypes: /image\/(jpeg|png|gif|bmp)/,
+
+				loadMethod: 'load',
+				// uploadUrl: uploadUrl,
+
+				fileToElement: function() {
+					var img = new CKEDITOR.dom.element( 'img' );
+					img.setAttribute( 'src', loadingImage );
+					return img;
+				},
+
+				parts: {
+					img: 'img'
+				},
+
+				onUploading: function( upload ) {
+					// Show the image during the upload.
+					this.parts.img.setAttribute( 'src', upload.data );
+				},
+
+				onUploaded: function( upload ) {
+					// Set width and height to prevent blinking.
+					this.replaceWith( '<img src="' + upload.url + '" ' +
+						'width="' + this.parts.img.$.naturalWidth + '" ' +
+						'height="' + this.parts.img.$.naturalHeight + '">' );
+				}
+			} );
+
+			editor.on("paste", onPaste, null, {editor: editor});
+		}
+	} );
+
+
+	function onPaste(event) {
+        var editor = event.listenerData && event.listenerData.editor;
+		var $event = event.data.dataTransfer.$;
+        var clipboardData = $event.items;
+        var found = false;
+        var imageType = /^image/;
+		var img = event.data.dataValue;
+
+		// For performance reason do not parse data if it does not contain img tag and data attribute.
+		if ( !img.match( /<img[\s\S]+data:/i ) ) {
+            return;
+        }
+
+        if (!clipboardData) {
+            return;
+        }
+
+        return Array.prototype.forEach.call(clipboardData, function (type, i) {
+            if (found) {
+                return;
+            }
+
+            if (type.type.match(imageType) || clipboardData[i].type.match(imageType)) {
+                readImageAsBase64(clipboardData[i], editor);
+                return found = true;
+            }
+        });
+    }
+
+    function readImageAsBase64(item, editor) {
+        if (!item || typeof item.getAsFile !== 'function') {
+            return;
+        }
+
+        var file = item.getAsFile();
+        var reader = new FileReader();
+
+        reader.onload = function (evt) {
+            var element = editor.document.createElement('img', {
+                attributes: {
+                    src: evt.target.result
+                }
+            });
+
+            // We use a timeout callback to prevent a bug where insertElement inserts at first caret
+            // position
+            setTimeout(function () {
+                editor.insertElement(element);
+            }, 10);
+        };
+
+        reader.readAsDataURL(file);
+    }
+
+	// jscs:disable maximumLineLength
+	// Black rectangle which is shown before image is loaded.
+	var loadingImage = 'data:image/gif;base64,R0lGODlhDgAOAIAAAAAAAP///yH5BAAAAAAALAAAAAAOAA4AAAIMhI+py+0Po5y02qsKADs=';
+	// jscs:enable maximumLineLength
+
+	/**
+	 * The URL where images should be uploaded.
+	 *
+	 * @since 4.5
+	 * @cfg {String} [imageUploadUrl='' (empty string = disabled)]
+	 * @member CKEDITOR.config
+	 */
+} )();
