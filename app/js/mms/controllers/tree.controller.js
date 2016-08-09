1ff40f1d6411a0f22cfefeec284e8c28b5e51a0e
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2016-08-09 15:02:52 -0700

    VE-342 - Grab all equations, figure, and tables in separate lists to display in tree view

--- app/js/mms/controllers/tree.controller.js
+++ app/js/mms/controllers/tree.controller.js
@@ -152,15 +152,23 @@ function($anchorScroll, $q, $filter, $location, $uibModal, $scope, $rootScope, $
 
     $scope.tableList = [];
     $scope.figureList = [];
+    $scope.equationList = [];
     $scope.$on('tree-show-tablesAndFigures', function() {
         $scope.showTandF = true;
         getPeTreeList(viewId2node[document.sysmlid], 'table',  $scope.tableList);
         getPeTreeList(viewId2node[document.sysmlid], 'figure', $scope.figureList);
+        getPeTreeList(viewId2node[document.sysmlid], 'equation', $scope.equationList);
     });
 
     // Get a list of specific PE type from branch
     function getPeTreeList(branch, type, list) {
+        if (type === 'equation' && branch.type === 'figure') { // Special case for equation as it is grouped with type 'figure'
+            if (branch.data.specialization.classifier[0] === ViewService.TYPE_TO_CLASSIFIER_ID.Equation)
+                list.push(branch);
+        }
         if ( branch.type === type) {
+            if ( (type == 'figure' && branch.data.specialization.classifier[0] != ViewService.TYPE_TO_CLASSIFIER_ID.Equation) ||
+                 (type == 'table'  && branch.data.specialization.classifier[0] != ViewService.TYPE_TO_CLASSIFIER_ID.Equation) )
             list.push(branch);
         }
         for (var i = 0; i < branch.children.length; i++) {
