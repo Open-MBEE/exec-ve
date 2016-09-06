3ac0353b9c50497190b495bf8cfeb38fcc5898a5
Dan Karlsson <dan.e.karlsson@jpl.nasa.gov>
2016-09-06 10:53:56 -0700

    Had the test working partially but it's still not responding correctly with the right element.

--- test/develop/unit/DirectiveSpecs/mmsViewSpec.js
+++ test/develop/unit/DirectiveSpecs/mmsViewSpec.js
@@ -3,43 +3,56 @@
 describe('mmsTranscludeVal directive', function () {
     var scope; //scope when directive is called
     var element; //dom element mms-transclude-name
-    var $rootScope, $compile, CacheService, UtilsService, $httpBackend, requestHandler;
+    var $rootScope,
+        $compile,
+        CacheService,
+        UtilsService,
+        $httpBackend,
+        ViewService,
+        URLService,
+        httpService,
+        $http;
 
     beforeEach(function () {
         module('mms.directives');
+        module('mms');
         inject(function ($injector) {
             $rootScope   = $injector.get('$rootScope');
             $compile     = $injector.get('$compile');
             $httpBackend = $injector.get('$httpBackend');
             CacheService = $injector.get('CacheService');
             UtilsService = $injector.get('UtilsService');
+            ViewService  = $injector.get('ViewService');
+            URLService   = $injector.get('URLService');
+            httpService  = $injector.get('HttpService');
+            $http        = $injector.get('$http');
             scope        = $rootScope.$new();
 
-            scope.view = {
-                sysmlid       : "301",
-                specialization: {
-                    type             : "View",
-                    displayedElements: ["301", "302", "303"],
-                    allowedElements  : ["301", "302", "303"],
-                    childrenViews    : ["304"],
-                    contains         : [{
-                        type          : "Paragraph",
-                        sourceType    : "reference",
-                        source        : "301",
-                        sourceProperty: "documentation"
+            var testElement = {
+                    elements: [{
+                        sysmlid       : "304",
+                        sectionNumber : "3241",
+                        version       : "666",
+                        workspace     : "master",
+                        specialization: {
+                            type             : "View",
+                            displayedElements: ["301", "302", "303"],
+                            allowedElements  : ["301", "302", "303"],
+                            childrenViews    : ["304"],
+                            contains         : [{
+                                type          : "Paragraph",
+                                sourceType    : "reference",
+                                source        : "301",
+                                sourceProperty: "documentation"
+                            }]
+                        }
                     }]
-                }
+                };
+            $httpBackend.when('GET', /\/alfresco\/(.+)?/)
+                .respond(testElement, {'A-Token': 'xxx'});
 
-            };
-
-
-            var cacheKey   = UtilsService.makeElementKey(scope.view.sysmlid, 'master', 'latest', false);
-            CacheService.put(cacheKey, scope.view);
-
-
-            element = angular.element('<mms-view data-mms-eid="{{view.sysmlid}}"></mms-view>');
-            $compile(element)(scope);
-            scope.$digest();
+            var cacheKey = UtilsService.makeElementKey(testElement.elements[0].sysmlid, 'master', 'latest', false);
+            CacheService.put(cacheKey, testElement);
         });
     });
 
@@ -50,8 +63,20 @@ describe('mmsTranscludeVal directive', function () {
 
     // TODO: NEEDS TO BE FINISHED
     it('mmsView should transclude a basic view', inject(function () {
-        // console.log(element);
-        // console.log(element.find('mms-transclude-name').first());
+        scope.view = {
+            sysmlid      : "304",
+            sectionNumber: "3241",
+            workspace    : "master"
+        };
+
+        // console.log(scope.view);
+
+        element = angular.element('<mms-view mms-vid="{{view.sysmlid}}" mms-version="{{view.version}}" mms-ws="{{view.ws}}" ></mms-view>');
+
+        $compile(element)(scope);
+
+        scope.$apply();
         console.log(element.html());
+        $httpBackend.flush();
     }));
 });
