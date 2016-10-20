18e6783ff011301464ab79d06e61d269ef9b168d
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2016-10-20 09:38:55 -0700

    up version

--- Gruntfile.js
+++ Gruntfile.js
@@ -277,7 +277,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: 'gov.nasa.jpl:evm:zip',
-            version: '2.4.1',
+            version: '2.4.2',
             path: 'deploy/'
           }]
         }
