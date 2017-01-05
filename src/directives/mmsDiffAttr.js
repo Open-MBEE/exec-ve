9c61b7903b4bbbbacee6bff9e249307d18180381
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-01-05 13:14:41 -0800

    update diff directive to compile and compare plain text

--- src/directives/mmsDiffAttr.js
+++ src/directives/mmsDiffAttr.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mms.directives')
-.directive('mmsDiffAttr', ['ElementService', 'ConfigService', 'URLService','$q', mmsDiffAttr]);
+.directive('mmsDiffAttr', ['ElementService', 'ConfigService', 'URLService','$q', '$compile', '$rootScope', '$interval', mmsDiffAttr]);
 
 /**
  * @ngdoc directive
@@ -22,7 +22,7 @@ angular.module('mms.directives')
  * @param {string=latest} mmsVersionOne  can be 'latest', timestamp or tag id, default is latest
  * @param {string=latest} mmsVersionTwo  can be 'latest', timestamp or tag id, default is latest
  */
-function mmsDiffAttr(ElementService, ConfigService, URLService, $q) {
+function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $rootScope, $interval) {
 
     var mmsDiffAttrLink = function(scope, element, attrs, mmsViewCtrl) {
         // TODO: error checking for missing elements -- util function for http error?? 
@@ -49,10 +49,43 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q) {
         };
         
         // Get the text to compare for diff
-        var getComparsionText = function(version){
+        var getComparsionText = function(ts){
             var deferred = $q.defer();
-            ElementService.getElement(scope.mmsEid, false, ws, version).then(function(data){
-                deferred.resolve(findElemType(data));
+            ElementService.getElement(scope.mmsEid, false, ws, ts).then(function(data){
+                var htmlData = angular.element(findElemType(data));
+
+                // inject workspace and timestamp - check for data-mms-* and mms-*
+                htmlData.find("mms-transclude-doc").each(function() {
+                    var transcludeElm = angular.element(this);
+                    if ( !transcludeElm.attr('mms-ws') || !transcludeElm.attr('data-mms-ws') ) {
+                        transcludeElm.attr("mms-ws", ws);
+                    }
+                    if ( !transcludeElm.attr('mms-version') || !transcludeElm.attr('data-mms-version') ) {
+                        transcludeElm.attr("mms-version", ts);
+                    }
+                });
+                htmlData.find("mms-transclude-name").each(function() {
+                    var transcludeElm = angular.element(this);
+                    if ( !transcludeElm.attr('mms-ws') || !transcludeElm.attr('data-mms-ws') ) {
+                        transcludeElm.attr("mms-ws", ws);
+                    }
+                    if ( !transcludeElm.attr('mms-version') || !transcludeElm.attr('data-mms-version') ) {
+                        transcludeElm.attr("mms-version", ts);
+                    }
+                });
+                
+                htmlData.find("mms-transclude-val").each(function() {
+                    var transcludeElm = angular.element(this);
+                    if ( !transcludeElm.attr('mms-ws') || !transcludeElm.attr('data-mms-ws') ) {
+                        transcludeElm.attr("mms-ws", ws);
+                    }
+                    if ( !transcludeElm.attr('mms-version') || !transcludeElm.attr('data-mms-version') ) {
+                        transcludeElm.attr("mms-version", ts);
+                    }
+                });
+
+                $compile(htmlData)($rootScope.$new());
+                deferred.resolve(htmlData);
             }, function(reason) {
                 element.html('<span class="mms-error">'+reason.message+'</span>');
                 deferred.reject(null);
@@ -87,20 +120,37 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q) {
         // example http://localhost:9000/mms.html#/workspaces/master/sites/vetest/documents/_17_0_5_1_407019f_1402422683509_36078_16169/views/_17_0_5_1_407019f_1402422692412_131628_16263
         var versionOne;
         var versionTwo;
-        tagOrTimestamp(scope.mmsVersionOne).then(function(data){
-            versionOne = data;
-            getComparsionText(versionOne).then(function(data){
-                scope.origElem = data;
+        // scope.htmlv1 = null;
+        // scope.htmlv2 = null;
+
+        // scope.$watch('htmlv1', function() {
+        //     scope.origElem = angular.element(scope.htmlv1).text();
+        // });
+        // scope.$watch('htmlv2', function() {
+        //     scope.compElem = angular.element(scope.htmlv2).text();
+        // });
+        tagOrTimestamp(scope.mmsVersionOne).then(function(versionOrTs){
+            getComparsionText(versionOrTs).then(function(data){
+                scope.origElem = angular.element(data).text();
+                // run on interval to check when data gets changed. once it changes set to origElem and break out of interval
+                 $interval(
+                    function(){
+                        scope.origElem = angular.element(data).text();
+                        console.log("here is the changed text: " +scope.origElem);
+                    }, 5000);
             }, function(reject){
                 scope.origElem = reject;
             });
         }, function(reject){
             element.html('<span class="mms-error">Version one not a valid tag or timestamp</span>');
         });
-        tagOrTimestamp(scope.mmsVersionTwo).then(function(data){
-            versionTwo = data;
-            getComparsionText(versionTwo).then(function(data){
-                scope.compElem = data;
+        tagOrTimestamp(scope.mmsVersionTwo).then(function(versionOrTs){
+            getComparsionText(versionOrTs).then(function(data){
+                scope.compElem = angular.element(data).text();
+                $interval(
+                    function(){
+                        scope.compElem = angular.element(data).text();
+                    }, 5000);
             }, function(reject){
                 scope.compElem = reject;
             });
