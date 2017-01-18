c1dbc21b85f29ab3f7d97ec2f2a38f7b91f44887
syferris <shadeh.y.ferris-francis@jpl.nasa.gov>
2017-01-18 12:14:09 -0800

    Added temporal diff feature that compares workspaces, timestamps/tags, and versions https://cae-jira.jpl.nasa.gov/browse/VE-452

--- src/directives/mmsDiffAttr.js
+++ src/directives/mmsDiffAttr.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mms.directives')
-.directive('mmsDiffAttr', ['ElementService', 'ConfigService', 'URLService','$q', '$compile', '$rootScope', '$interval', mmsDiffAttr]);
+.directive('mmsDiffAttr', ['ElementService', 'WorkspaceService','ConfigService', 'URLService','$q', '$compile', '$rootScope', '$interval', mmsDiffAttr]);
 
 /**
  * @ngdoc directive
@@ -12,9 +12,9 @@ angular.module('mms.directives')
  * @restrict E
  *
  * @description
- *  Compares a element at two different times and generates a pretty diff. 
+ *  Compares a element at two different times and generates a pretty diff.
  * ## Example
- * 
+ *
  * <mms-diff-attr mms-eid="element-id" mms-attr="name/doc/val" mms-version-one="timestamp/latest/tag?" mms-version-two="timestamp/latest/tag?"></mms-diff-attr>
  *
  * @param {string} mmsEid The id of the element whose doc to transclude
@@ -22,19 +22,50 @@ angular.module('mms.directives')
  * @param {string=latest} mmsVersionOne  can be 'latest', timestamp or tag id, default is latest
  * @param {string=latest} mmsVersionTwo  can be 'latest', timestamp or tag id, default is latest
  */
-function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $rootScope, $interval) {
+function mmsDiffAttr(ElementService, WorkspaceService, ConfigService, URLService, $q, $compile, $rootScope, $interval) {
 
     var mmsDiffAttrLink = function(scope, element, attrs, mmsViewCtrl) {
-        // TODO: error checking for missing elements -- util function for http error?? 
-        var ws = scope.mmsWs;
+        // TODO: error checking for missing elements -- util function for http error??
+        var wsOne = scope.mmsWsOne;
+        var wsTwo = scope.mmsWsTwo;
+        var wsOneFlag = false;
+        var wsTwoFlag = false;
+
         if (mmsViewCtrl) {
             var viewVersion = mmsViewCtrl.getWsAndVersion();
-            if (!ws)
-                ws = viewVersion.workspace;
-        }
+
+            if (!wsOne) {
+                wsOne = viewVersion.workspace; //ws one and two
+            }
+            else {
+                WorkspaceService.getWorkspace(wsOne).then(function(data) {
+              }, function(reason) {
+                wsOneFlag = true;
+                element.html('<span class="mms-error">Workspace One does not exist.</span>');
+              });
+            }
+
+            if (!wsTwo) {
+                wsTwo = viewVersion.workspace;
+            }
+            else {
+                WorkspaceService.getWorkspace(wsTwo).then(function(data) {
+              }, function(reason) {
+                wsTwoFlag = true;
                 
-        // Check if input is a tag, timestamp or neither        
-        var tagOrTimestamp = function(version){
+                if(wsOneFlag && wsTwoFlag)
+                  element.html('<span class="mms-error">Workspace One & Two do not exist.</span>');
+                else {
+                  element.html('<span class="mms-error">Workspace Two does not exist.</span>');
+                }
+              });
+            }
+
+
+          }
+
+        // Check if input is a tag, timestamp or neither
+        var tagOrTimestamp = function(version, ws){
             var deferred = $q.defer();
             if(!URLService.isTimestamp(version) && version !== 'latest'){
                 ConfigService.getConfig(version, ws, false).then(function(data){
@@ -47,20 +78,20 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $r
             }
             return deferred.promise;
         };
-        
+
         // Get current element and update to use proper ws and ts if not already defined in html
-        var setVersionWs = function(elt, ts){
+        var setVersionWs = function(elt, ts, ws){
             var transcludeElm = angular.element(elt);
             if ( !transcludeElm.attr('mms-ws') && !transcludeElm.attr('data-mms-ws') ) {
                 transcludeElm.attr("mms-ws", ws);
             }
             if ( !transcludeElm.attr('mms-version') && !transcludeElm.attr('data-mms-version') ) {
-                transcludeElm.attr("mms-version", ts);
+                transcludeElm.attr("mms-version", ts); //version one and two
             }
         };
 
         // Get the text to compare for diff
-        var getComparsionText = function(ts){
+        var getComparsionText = function(ts, ws){
             var deferred = $q.defer();
             ElementService.getElement(scope.mmsEid, false, ws, ts).then(function(data){
                 var htmlData = angular.element(findElemType(data));
@@ -73,7 +104,7 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $r
                 htmlData.find("mms-transclude-name").each(function() {
                     setVersionWs(this, ts);
                 });
-                
+
                 htmlData.find("mms-transclude-val").each(function() {
                     setVersionWs(this, ts);
                 });
@@ -85,7 +116,7 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $r
             });
             return deferred.promise;
         };
-        
+
         // Find the right key to fetch text
         var findElemType = function(elem){
             if(scope.mmsAttr === 'name'){//the key is included and blank
@@ -107,16 +138,16 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $r
                     element.html('<span class="mms-error">Value type not supported for now</span>');
                     return null;
                 }
-                
-            }            
+
+            }
         };
         // example http://localhost:9000/mms.html#/workspaces/master/sites/vetest/documents/_17_0_5_1_407019f_1402422683509_36078_16169/views/_17_0_5_1_407019f_1402422692412_131628_16263
         var data1CheckForBreak = false;
         var data2CheckForBreak = false;
 
 
-        tagOrTimestamp(scope.mmsVersionOne).then(function(versionOrTs){
-            getComparsionText(versionOrTs).then(function(data){
+        tagOrTimestamp(scope.mmsVersionOne, scope.mmsWsOne).then(function(versionOrTs){ //wsOne
+            getComparsionText(versionOrTs, scope.mmsWsOne).then(function(data){
                 scope.origElem = angular.element(data).text();
                 // run on interval to check when data gets changed. once it changes set to origElem and break out of interval
                  var promise1 = $interval(
@@ -137,8 +168,8 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $r
         }, function(reject){
             element.html('<span class="mms-error">Version one not a valid tag or timestamp</span>');
         });
-        tagOrTimestamp(scope.mmsVersionTwo).then(function(versionOrTs){
-            getComparsionText(versionOrTs).then(function(data){
+        tagOrTimestamp(scope.mmsVersionTwo, scope.mmsWsTwo).then(function(versionOrTs){
+            getComparsionText(versionOrTs, scope.mmsWsTwo).then(function(data){
                 scope.compElem = angular.element(data).text();
                 var promise2 = $interval(
                     function(){
@@ -164,8 +195,9 @@ function mmsDiffAttr(ElementService, ConfigService, URLService, $q, $compile, $r
         restrict: 'E',
         scope: {
             mmsEid: '@',
-            mmsWs: '@',
-            mmsAttr: '@',
+            mmsWsOne: '@', //ws 1 and 2
+            mmsWsTwo: '@',
+            mmsAttr: '@', //name or doc
             mmsVersionOne: '@',
             mmsVersionTwo: '@'
         },
