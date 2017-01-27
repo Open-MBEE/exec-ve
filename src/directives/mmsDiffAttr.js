4900595992f0d780ba13f128d521d9a76a9be842
syferris <shadeh.y.ferris-francis@jpl.nasa.gov>
2017-01-27 10:53:12 -0800

    Refined code for checking if element and versions exists

--- src/directives/mmsDiffAttr.js
+++ src/directives/mmsDiffAttr.js
@@ -144,34 +144,34 @@ function mmsDiffAttr(ElementService, WorkspaceService, ConfigService, URLService
 
         var checkElement = function(origNotFound, compNotFound, deletedFlag){
           switch(origNotFound){
-            case 0:
-              if (compNotFound == 1)
-                element.html('<span class="mms-error">This element has been removed from the View. </span>');
-              else if (deletedFlag == 1)
-                element.prepend('<span class="mms-error">This element has been deleted: </span>');
+            case false:
+              if (compNotFound === true)
+                element.html('<span class="mms-error"> This element has been removed from the View. </span>');
+              else if (deletedFlag === true)
+                element.prepend('<span class="mms-error"> This element has been deleted: </span>');
               break;
             default:
-              if (compNotFound === 0){
+              if (compNotFound === false){
                 if (scope.compElem === "")
                   element.html('<span class="mms-error"> This element is a new element with no content. </span>');
                 else
                   element.prepend('<span class="mms-error"> This element is a new element: </span>');
               }
-              else if (compNotFound == 1)
-                element.html('<span class="mms-error">This element does not exist at either point in time. </span>');
+              else if (compNotFound === true)
+                element.html('<span class="mms-error"> This element does not exist at either point in time. </span>');
           }
         };
 
         var checkVersion = function(vrOneInvalidFlag, vrTwoInvalidFlag){
           switch(vrOneInvalidFlag){
-            case 0:
-              if (vrTwoInvalidFlag == 1)
+            case false:
+              if (vrTwoInvalidFlag === true)
                 element.html('<span class="mms-error"> Version Two not a valid tag/timestamp. </span>');
               break;
             default:
-              if(vrTwoInvalidFlag === 0)
+              if(vrTwoInvalidFlag === false)
                 element.html('<span class="mms-error"> Version One not a valid tag/timestamp. </span>');
-              else if(vrTwoInvalidFlag == 1)
+              else if(vrTwoInvalidFlag === true)
                 element.html('<span class="mms-error"> Version One and Version Two not valid tags/timestamps. ');
           }
         };
@@ -179,11 +179,11 @@ function mmsDiffAttr(ElementService, WorkspaceService, ConfigService, URLService
         // example http://localhost:9000/mms.html#/workspaces/master/sites/vetest/documents/_17_0_5_1_407019f_1402422683509_36078_16169/views/_17_0_5_1_407019f_1402422692412_131628_16263
         var data1CheckForBreak = false;
         var data2CheckForBreak = false;
-        var vrOneInvalidFlag = 0;
-        var vrTwoInvalidFlag = 0;
-        var origNotFound = 0;
-        var compNotFound = 0;
-        var deletedFlag = 0;
+        var vrOneInvalidFlag = false;
+        var vrTwoInvalidFlag = false;
+        var origNotFound = false;
+        var compNotFound = false;
+        var deletedFlag = false;
 
         tagOrTimestamp(scope.mmsVersionOne, scope.mmsWsOne).then(function(versionOrTs){
             getComparsionText(versionOrTs, scope.mmsWsOne).then(function(data){
@@ -204,12 +204,12 @@ function mmsDiffAttr(ElementService, WorkspaceService, ConfigService, URLService
             }, function(reject){
                 scope.origElem = reject;
                 if(reject.toLowerCase() == "not found") {
-                  origNotFound = 1;
+                  origNotFound = true;
                   scope.origElem = '';
                 }
             });
         }, function(reject){
-            vrOneInvalidFlag = 1;
+            vrOneInvalidFlag = true;
         });
 
         tagOrTimestamp(scope.mmsVersionTwo, scope.mmsWsTwo).then(function(versionOrTs){
@@ -227,18 +227,19 @@ function mmsDiffAttr(ElementService, WorkspaceService, ConfigService, URLService
                         scope.compElem = angular.element(data).text();
                         // console.log("here is the changed text: " +scope.compElem);
                     }, 5000);
+                    checkElement(origNotFound, compNotFound, deletedFlag);
             }, function(reject){
                 scope.compElem = reject;
                 scope.compElem = '';
-                if(reject.toLowerCase() == "not found")
-                  compNotFound = 1;
-                else if(reject.toLowerCase() == "deleted")
-                  deletedFlag = 1;
-                elementCheck(origNotFound, compNotFound, deletedFlag);
+                if(reject.toLowerCase() == "not found"){
+                  compNotFound = true;
+                }else if(reject.toLowerCase() == "deleted")
+                  deletedFlag = true;
+                checkElement(origNotFound, compNotFound, deletedFlag);
             });
         }, function(reject){
-            vrTwoInvalidFlag = 1;
-            versionCheck(vrOneInvalidFlag, vrTwoInvalidFlag);
+            vrTwoInvalidFlag = true;
+            checkVersion(vrOneInvalidFlag, vrTwoInvalidFlag);
         });
     };
 
