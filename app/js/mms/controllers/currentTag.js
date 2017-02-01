18cd2bc9bd8999908e5fc735e93e2a02a707886e
syferris <shadeh.y.ferris-francis@jpl.nasa.gov>
2017-02-01 09:27:38 -0800

    Created directive to get the current tag in which the view resides

--- app/js/mms/controllers/currentTag.js
+++ app/js/mms/controllers/currentTag.js
@@ -3,12 +3,25 @@
 angular.module('mmsApp')
 .directive('currentTag',['$rootScope', currentTag]);
 
+/**
+* @ngdoc directive
+* @name mmsApp.directive:currentTag
+*
+* @restrict E
+*
+* @description
+* Displays the current tag in which the view resides
+*
+* ##Example
+* <current-tag></current-tag>
+*
+*/
 function currentTag($rootScope){
 
   var currentTagLink = function(scope, element, attrs){
     var tag = $rootScope.mms_tag;
-    if (!tag)
-      scope.tag = 'latest';
+    if (!tag || tag.name === 'latest') //if undefined (latest may be undefined)
+      scope.tag = 'Latest';
     else {
       scope.tag = tag.name;
     }
@@ -16,6 +29,7 @@ function currentTag($rootScope){
 
   return {
     restrict: 'E',
+    scope: {},
     template: 'Current Tag: {{ tag }}',
     link: currentTagLink
   };
