7caac86d89e1bd0f99dc9cf8df66fe3d2d014c13
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-02-09 17:20:10 -0800

    reorder directives based on app/mms

--- app/js/mms/directives/mmsFooter.js
+++ app/js/mms/directives/mmsFooter.js
@@ -4,11 +4,11 @@ angular.module('mms.directives')
 .directive('mmsFooter', ['$templateCache', mmsFooter]);
 
 function mmsFooter($templateCache) {
-	var template = $templateCache.get('mms/templates/mmsFooter.html');
-	
-	return {
-		restrict: 'E', 
-		template: template,
+  var template = $templateCache.get('mms/templates/mmsFooter.html');
+
+  return {
+    restrict: 'E',
+    template: template,
         transclude: true
-	};
+  };
 }
