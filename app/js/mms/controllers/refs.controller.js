95c5889b591e9435c9c58f7836e51dc63b7040f5
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-03-29 21:58:28 -0700

    VE-577 fix deleting refs and cleanup code

--- app/js/mms/controllers/refs.controller.js
+++ app/js/mms/controllers/refs.controller.js
@@ -18,6 +18,7 @@ function($q, $filter, $location, $uibModal, $scope, $rootScope, $state, $timeout
     $scope.refList = refObs;
     $scope.branches = branchObs;
     $scope.tags = tagObs;
+    $scope.activeTab = 0;
     $scope.refSelected = null;
     $scope.search = null;
     $scope.view = null;
@@ -39,7 +40,9 @@ function($q, $filter, $location, $uibModal, $scope, $rootScope, $state, $timeout
     $scope.$on('tree-add-tag', function(e) {
         addItem('Tag');
     });
-
+    $scope.$on('tree-delete', function(e) {
+        deleteItem();
+    });
 
     $scope.refClickHandler = function(ref) {
         ProjectService.getRef(ref.id, projectOb.id).then(
@@ -71,66 +74,53 @@ function($q, $filter, $location, $uibModal, $scope, $rootScope, $state, $timeout
         $scope.itemType = itemType;
         var branch = $scope.refSelected;
         var templateUrlStr = "";
-    //     var newBranchType = "";
-        var branchType = "";
 
-    //      // Item specific setup:
+        // Item specific setup:
         if (itemType === 'Branch') {
             if (!branch) {
                 growl.warning("Add Task Error: Select a task or tag first");
                 return;
             }
             if (branch.type === 'Tag') {
-                // $scope.createWsParentId = branch.workspace;
-                // $scope.createWsTime = branch.timestamp;
                 $scope.from = 'Tag ' + branch.name;
             } else {
-                $scope.createWsParentId = branch.id;
-                $scope.createWsTime = $filter('date')(new Date(), 'yyyy-MM-ddTHH:mm:ss.sssZ');
                 $scope.from = 'Task ' + branch.name;
             }
+            $scope.createParentRefId = branch.id;
             templateUrlStr = 'partials/mms/new-task.html';
-            branchType = 'workspace';
-        }
-        else if (itemType === 'Tag') {
+        } else if (itemType === 'Tag') {
             if (!branch) {
                 growl.warning("Add Tag Error: Select parent task first");
                 return;
-            } else if (branch.type != "workspace") {
-                growl.warning("Add Tag Error: Selection must be a task");
-                return;
-            }
-            $scope.createConfigParentId = branch.data.id;
-            $scope.configuration = {};
-            $scope.configuration.now = true;
+            } 
+            // else if (branch.type != "workspace") {
+            //     growl.warning("Add Tag Error: Selection must be a task");
+            //     return;
+            // }
+            $scope.createParentRefId = branch.id;
             templateUrlStr = 'partials/mms/new-tag.html';
-            branchType = 'configuration';
         } else {
             growl.error("Add Item of Type " + itemType + " is not supported");
             return;
         }
-        $scope.parentBranchData = branch.data;
         // Adds the branch:
         var instance = $uibModal.open({
             templateUrl: templateUrlStr,
             scope: $scope,
             controller: ['$scope', '$uibModalInstance', '$filter', addItemCtrl]
         });
-    //     instance.result.then(function(data) {
-    //         var newbranch = {
-    //             label: data.name,
-    //             type: newBranchType,
-    //             data: data,
-    //             children: []
-    //         };
-            
-    //         var top = false; //TODO fix tags and branch
-    //         if (itemType === 'Document') {
-    //             newbranch.groupId = branch.data._id;
-    //         }
-    //         $scope.treeApi.add_branch(branch, newbranch, top);
-
-    //     });
+        instance.result.then(function(data) {
+            //TODO add to correct ref list
+            if (data.type === 'Branch') {
+                $scope.branches.push(data);
+                $scope.refSelected = data;
+                $scope.activeTab = 0;
+            } else {
+                $scope.tags.push(data);
+                $scope.refSelected = data;
+                $scope.activeTab = 1;
+            }
+        });
     };
 
     var addItemCtrl = function($scope, $uibModalInstance, $filter) {
@@ -138,17 +128,20 @@ function($q, $filter, $location, $uibModal, $scope, $rootScope, $state, $timeout
         $scope.oking = false;
         var displayName = "";
 
-        if ($scope.itemType === 'Document') {
-            $scope.newDoc = {name: ""};
-            displayName = "Document";
-        } else if ($scope.itemType === 'View') {
-            $scope.newView = {name: ''};
-            displayName = "View";
-        } else {
-            growl.error("Add Item of Type " + $scope.itemType + " is not supported");
-            return;
+        // Item specific setup:
+        if ($scope.itemType === 'Branch') {
+            $scope.workspace = {};
+            $scope.workspace.name = "";
+            $scope.workspace.description = "";
+            $scope.workspace.permission = "read";
+            displayName = "Branch";
         }
-       
+        else if ($scope.itemType === 'Tag') {
+            $scope.configuration = {};
+            $scope.configuration.name = "";
+            $scope.configuration.description = "";
+            displayName = "Tag";
+        }
 
         $scope.ok = function() {
             if ($scope.oking) {
@@ -158,38 +151,33 @@ function($q, $filter, $location, $uibModal, $scope, $rootScope, $state, $timeout
             $scope.oking = true;
             var promise;
 
-    //         // Item specific promise: //TODO branch and tags
-    //         if ($scope.itemType === 'Document') {
-    //             promise = ViewService.createDocument({
-    //                 _projectId: projectOb.id,
-    //                 _refId: refOb.id,
-    //                 id: $scope.parentBranchData._id
-    //             },{
-    //                 viewName: $scope.newDoc.name,
-    //                 isDoc: true
-    //             });
-    //         } else if ($scope.itemType === 'View') {
-    //             $scope.newViewAggr.type = "composite";
-    //             promise = ViewService.createView($scope.parentBranchData, {
-    //                 viewName: $scope.newView.name
-    //             });
-    //         } else {
-    //             growl.error("Add Item of Type " + $scope.itemType + " is not supported");
-    //             $scope.oking = false;
-    //             return;
-    //         }
+            // Item specific promise:
+            if ($scope.itemType === 'Branch') {
+                var branchObj = {"name": $scope.workspace.name, "type": "Branch", 
+                                "description": $scope.workspace.description};
+                branchObj.parentRefId = $scope.createParentRefId;
+                promise = ProjectService.createRef( branchObj, projectOb.id );
+            }
+            else if ($scope.itemType === 'Tag') {
+                var tagObj = {"name": $scope.configuration.name, "type": "Tag",
+                                "description": $scope.configuration.description};
+                tagObj.parentRefId =  $scope.createParentRefId;
+                promise = ProjectService.createRef( tagObj, projectOb.id );
+            } else {
+                growl.error("Add Item of Type " + $scope.itemType + " is not supported");
+                $scope.oking = false;
+                return;
+            }
 
-    //         promise.then(function(data) {
-    //             growl.success(displayName+" Created");
-    //             if ($scope.itemType === 'Tag') {
-    //                 growl.info('Please wait for a completion email prior to viewing of the tag.');
-    //             }
-    //             $uibModalInstance.close(data);
-    //         }, function(reason) {
-    //             growl.error("Create "+displayName+" Error: " + reason.message);
-    //         }).finally(function() {
-    //             $scope.oking = false;
-    //         });
+            promise.then(function(data) {
+                growl.success(displayName+" Created");
+                growl.info('Please wait for a completion email prior to viewing of the tag.');
+                $uibModalInstance.close(data);
+            }, function(reason) {
+                growl.error("Create "+displayName+" Error: " + reason.message);
+            }).finally(function() {
+                $scope.oking = false;
+            });
         };
 
         $scope.cancel = function() {
@@ -197,91 +185,62 @@ function($q, $filter, $location, $uibModal, $scope, $rootScope, $state, $timeout
         };
     };
 
-    // $scope.deleteItem = function() {
-    //     var branch = $scope.treeApi.get_selected_branch();
-    //     if (!branch) {
-    //         growl.warning("Delete Error: Select item to delete.");
-    //         return;
-    //     }
-    //     if ($state.includes('project.ref.document')) { 
-    //         if (branch.type !== 'view' || (!UtilsService.isView(branch.data))) {
-    //             growl.warning("Delete Error: Selected item is not a view.");
-    //             return;
-    //         }
-    //     }
-    //     if ($state.includes('project.ref') && !$state.includes('project.ref.document')) {
-    //         if (branch.type !== 'view' || (!UtilsService.isDocument(branch.data))) {
-    //             growl.warning("Delete Error: Selected item is not a document.");
-    //             return;
-    //         }
-    //     }
-    //     // TODO: do not pass selected branch in scope, move page to generic location
-    //     $scope.deleteBranch = branch;
-    //     var instance = $uibModal.open({
-    //         templateUrl: 'partials/mms/delete.html',
-    //         scope: $scope,
-    //         controller: ['$scope', '$uibModalInstance', deleteCtrl]
-    //     });
-    //     instance.result.then(function(data) {
-    //         $scope.treeApi.remove_branch(branch);
-    //         // if ($state.includes('project.ref.document') && branch.type === 'view') {
-    //         //     processDeletedViewBranch(branch);
-    //         // }
-    //         if ($state.includes('project.ref') && !$state.includes('project.ref.document')) {
-    //             return;
-    //         }
-    //         if ($rootScope.ve_fullDocMode) {
-    //             $state.go('project.ref.document.full', {search: undefined});
-    //             $state.reload();
-    //         } else {
-    //             $state.go('^', {search: undefined});
-    //         }
-    //     });
-    // };
+    var deleteItem = function() {
+        var branch = $scope.refSelected;
+        if (!branch) {
+            growl.warning("Delete Error: Select item to delete.");
+            return;
+        }
+        $scope.deleteBranch = branch;
+        var instance = $uibModal.open({
+            templateUrl: 'partials/mms/delete.html',
+            scope: $scope,
+            controller: ['$scope', '$uibModalInstance', deleteCtrl]
+        });
+        instance.result.then(function(data) {
+            //TODO $state project with no selected ref
+            var index;
+            if (data.type === 'Branch') {
+                index = $scope.branches.indexOf($scope.refSelected);
+                $scope.branches.splice(index, 1);  
+            } else if (data.type === 'Tag') {
+                index = $scope.tags.indexOf($scope.refSelected);
+                $scope.tags.splice(index, 1);  
+            }
+            $scope.refSelected = null;
+        });
+    };
 
-    // // TODO: Make this a generic delete controller
-    // var deleteCtrl = function($scope, $uibModalInstance) {
-    //     $scope.oking = false;
-    //     var branch = $scope.deleteBranch;
-    //     if (branch.type === 'view') {
-    //         $scope.type = 'View';
-    //         if (UtilsService.isDocument(branch.data))
-    //             $scope.type = 'Document';
-    //     }
-    //     $scope.name = branch.data.name;
-    //     $scope.ok = function() {
-    //         if ($scope.oking) {
-    //             growl.info("Please wait...");
-    //             return;
-    //         }
-    //         $scope.oking = true;
-    //         var promise = null;
-    //         if (branch.type === 'view') {
-    //             var parentBranch = $scope.treeApi.get_parent_branch(branch);
-    //             if (!$state.includes('project.ref.document')) {
-    //                 promise = ViewService.downgradeDocument(branch.data);
-    //             } else {
-    //                 promise = ViewService.removeViewFromParentView({
-    //                     projectId: parentBranch.data._projectId,
-    //                     refId: parentBranch.data._refId,
-    //                     parentViewId: parentBranch.data.id,
-    //                     viewId: branch.data.id
-    //                 });
-    //             }
-    //         }
-    //         promise.then(function(data) {
-    //             growl.success($scope.type + " Deleted");
-    //             $uibModalInstance.close('ok');
-    //         }, function(reason) {
-    //             growl.error($scope.type + ' Delete Error: ' + reason.message);
-    //         }).finally(function() {
-    //             $scope.oking = false;
-    //         });
-    //     };
-    //     $scope.cancel = function() {
-    //         $uibModalInstance.dismiss();
-    //     };
-    // };
+    var deleteCtrl = function($scope, $uibModalInstance) {
+        $scope.oking = false;
+        var branch = $scope.deleteBranch;
+        if (branch.type === 'Tag') {
+            $scope.type = 'Tag';
+        } else if (branch.type === 'Branch') {
+            $scope.type = 'Branch';
+        }
+        $scope.name = branch.name;
+        $scope.ok = function() {
+            if ($scope.oking) {
+                growl.info("Please wait...");
+                return;
+            }
+            $scope.oking = true;
+            var promise = ProjectService.deleteRef(branch.id, projectOb.id);
+            promise.then(function(data) {
+                growl.success($scope.type + " Deleted");
+                $uibModalInstance.close('ok');
+            }, function(reason) {
+                growl.error($scope.type + ' Delete Error: ' + reason.message);
+            }).finally(function() {
+                $scope.oking = false;
+            });
+        };
+
+        $scope.cancel = function() {
+            $uibModalInstance.dismiss();
+        };
+    };
 
 
 }]);
