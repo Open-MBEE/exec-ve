a566ae50a5222fee598e8c72d996d7e6a51986f0
syferris <shadeh.y.ferris-francis@jpl.nasa.gov>
2017-04-06 11:59:44 -0700

    Conversion for 3.0.0

--- src/services/ViewService.js
+++ src/services/ViewService.js
@@ -235,11 +235,11 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
     var getViewElements = function(reqOb, weight, update) {
         UtilsService.normalize(reqOb);
         var deferred = $q.defer();
-        var key = 'viewElements' + reqOb.projectId + reqOb.refId + reqOb.elementId + reqOb.commitId;
+        var key = 'viewElements' + reqOb._projectId + reqOb._refId + reqOb.id + reqOb._commitId;
         if (inProgress[key]) {
             return inProgress[key];
         }
-        var requestCacheKey = ['elements', reqOb.projectId, reqOb.refId, reqOb.elementId, reqOb.commitId];
+        var requestCacheKey = ['elements', reqOb._projectId, reqOb._refId, reqOb.id, reqOb._commitId];
         var cached = CacheService.get(requestCacheKey);
         if (cached && !update) {
             deferred.resolve(cached);
@@ -292,7 +292,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
         UtilsService.normalize(reqOb);
         var deferred = $q.defer();
         var url = URLService.getDocumentViewsURL(reqOb);
-        var cacheKey = ['views', reqOb.projectId, reqOb.refId, reqOb.elementId];
+        var cacheKey = ['views', reqOb._projectId, reqOb._refId, reqOb.id];
         if (CacheService.exists(cacheKey) && !update) {
             deferred.resolve(CacheService.get(cacheKey));
         } else {
@@ -323,9 +323,9 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
         UtilsService.normalize(reqOb);
         var deferred = $q.defer();
         ElementService.getElement({
-            projectId: reqOb.projectId,
-            refId: reqOb.refId,
-            elementId: reqOb.parentViewId,
+            projectId: reqOb._projectId,
+            refId: reqOb._refId,
+            elementId: reqOb.ownerId,
             extended: true
         }, 2).then(function(data) {  
             var clone = {
@@ -340,7 +340,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
             } else {
                 clone._childViews = [];
             }
-            clone._childViews.push({id: reqOb.viewId, aggregation: reqOb.aggr});
+            clone._childViews.push({id: reqOb.id, aggregation: reqOb.aggregation});
             ElementService.updateElement(clone)
             .then(function(data2) {
                 deferred.resolve(data2);
@@ -368,9 +368,9 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
         UtilsService.normalize(reqOb);
         var deferred = $q.defer();
         ElementService.getElement({
-            projectId: reqOb.projectId,
-            refId: reqOb.refId,
-            elementId: reqOb.parentViewId,
+            projectId: reqOb._projectId,
+            refId: reqOb._refId,
+            elementId: reqOb.ownerId,
             extended: true
         }, 2).then(function(data) {  
             if (data._childViews) {
@@ -383,7 +383,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
                     _childViews: JSON.parse(JSON.stringify(data._childViews))
                 };
                 for (var i = 0; i < clone._childViews.length; i++) {
-                    if (clone._childViews[i].id === reqOb.viewId) {
+                    if (clone._childViews[i].id === reqOb.id) {
                         clone._childViews.splice(i,1);
                         break; 
                     }
@@ -616,7 +616,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
     var createView = function(ownerOb, viewOb) {
         var deferred = $q.defer();
 
-        var newViewId = viewOb.viewId ? viewOb.viewId : UtilsService.createMmsId();
+        var newViewId = viewOb.id ? viewOb.id : UtilsService.createMmsId();
         var newInstanceId = '_hidden_' + UtilsService.createMmsId() + '_pei';
 
         var view = {
@@ -634,8 +634,8 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
                 }],
                 type: 'Expression'
             },
-            name: viewOb.viewName ? viewOb.viewName : 'Untitled View',
-            documentation: viewOb.viewDoc ? viewOb.viewDoc : '',
+            name: viewOb.name ? viewOb.name : 'Untitled View',
+            documentation: viewOb.documentation ? viewOb.documentation : '',
             _appliedStereotypeIds: [
                 (viewOb.isDoc ? "_17_0_2_3_87b0275_1371477871400_792964_43374" : "_17_0_1_232f03dc_1325612611695_581988_21583")
             ],
@@ -751,7 +751,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
         UtilsService.normalize(reqOb);
         var deferred = $q.defer();
         var url = URLService.getProjectDocumentsURL(reqOb);
-        var cacheKey = ['documents', reqOb.projectId, reqOb.refId];
+        var cacheKey = ['documents', reqOb._projectId, reqOb._refId];
         if (CacheService.exists(cacheKey) && !update) {
             deferred.resolve(CacheService.get(cacheKey));
         } else {
@@ -785,7 +785,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
         var type = instanceSpecSpec.type;
 
         if (type === 'LiteralString') { // If it is a Opaque List, Paragraph, Table, Image, List:
-            var jsonString = instanceSpecSpec.value;
+            var jsonString = instanceSpecSpec.operand.instanceId;
             return JSON.parse(jsonString); 
         } else if (type === 'Expression') { // If it is a Opaque Section, or a Expression:
             // If it is a Opaque Section then we want the instanceSpec:
@@ -941,7 +941,7 @@ function ViewService($q, $http, $rootScope, URLService, ElementService, UtilsSer
                 return;
             }
             for (var i = 0; i < data.length; i++) {
-                var prop = data[i];
+                var prop = data[i]; //is this now 'slot'?
                 var feature = prop.definingFeatureId ? prop.definingFeatureId : null;
                 var value = prop.value ? prop.value : null;
                 if (!feature || !docMetadataTypes[feature] || !value || value.length === 0) {
