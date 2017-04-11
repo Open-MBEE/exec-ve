3c875e7eebb27bfffd1ecf3a1dc2ed8497dab0d9
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-04-11 15:49:24 -0700

    VE-588 - update stomp for donbot - use element.updated listener

--- src/services/StompService.js
+++ src/services/StompService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('mms')
-.factory('StompService', ['$rootScope', 'UtilsService', '$window', '$location','ApplicationService', 'CacheService', 'URLService','$http', StompService]);
+.factory('StompService', ['$rootScope', 'ApplicationService', 'ElementService', 'UtilsService', 'CacheService', 'URLService','$http', StompService]);
 
 /**
  * @ngdoc service
@@ -11,7 +11,7 @@ angular.module('mms')
  * @description
  * Provides messages from the activemq JMS bus
  */
-function StompService($rootScope, UtilsService, $window, $location, ApplicationService, CacheService, URLService, $http) {
+function StompService($rootScope, ApplicationService, ElementService, UtilsService,  CacheService, URLService, $http) {
      var stompClient = {};
      var host;
 
@@ -29,29 +29,38 @@ function StompService($rootScope, UtilsService, $window, $location, ApplicationS
 
     var stompSuccessCallback = function(message){
         var updateWebpage = angular.fromJson(message.body);
-        var workspaceId = message.headers.workspace;
+        var projectId = message.headers.projectId;
+        var refId = message.headers.refId;
         if(updateWebpage.source !== ApplicationService.getSource()){
             $rootScope.$apply( function(){
                 if(updateWebpage.refs.addedElements && updateWebpage.refs.addedElements.length > 0){
-                    angular.forEach( updateWebpage.refs.addedElements, function(value, key) {
-                        // check if element is in the cache, if not ignore
-                        //var ws = !workspace ? 'master' : workspace;
-                        var inCache = CacheService.exists( UtilsService.makeElementKey(value.id, workspaceId, 'latest', false) );
-                        if(inCache === true)
-                            UtilsService.mergeElement(value, value.id, workspaceId, false, "all" );
-                        $rootScope.$broadcast("stomp.element", value, workspaceId, value.id , value._modifier, value.name);
+                    angular.forEach( updateWebpage.refs.addedElements, function(eltId) {
+                        refId = !refId ? 'master' : refId;
+                        ElementService.getElement({
+                            projectId: projectId,
+                            refId: refId,
+                            extended: true,
+                            elementId: eltId
+                        }, 1, true).then(function(data){
+                            $rootScope.$broadcast("stomp.element", data);
+                        }, function(reason) {
+                            // growl.error("Getting Element Error: " + reason.message);
+                        });
                     });
                 }
                 if(updateWebpage.refs.updatedElements && updateWebpage.refs.updatedElements.length > 0){
-                    angular.forEach( updateWebpage.refs.updatedElements, function(value, key) {
+                    angular.forEach( updateWebpage.refs.updatedElements, function(eltId) {
                         // TODO fix - only sends back id.. do we need to make a call to get obj?
-                        // var inCache = CacheService.exists( UtilsService.makeElementKey(value.id, workspaceId, 'latest', false) );
-                        // if(inCache === true && $rootScope.veEdits && $rootScope.veEdits['element|' + value.id + '|' + workspaceId] === undefined)
-                        //     UtilsService.mergeElement(value, value.id, workspaceId, false, "all" );
-                        // var history = CacheService.get(UtilsService.makeElementKey(value.id, workspaceId, 'versions'));
-                        // if (history)
-                        //     history.unshift({modifier: value._modifier, timestamp: value._modified});
-                        // $rootScope.$broadcast("stomp.element", value, workspaceId, value.id , value._modifier, value.name);
+                        ElementService.getElement({
+                            projectId: projectId,
+                            refId: refId,
+                            extended: true,
+                            elementId: eltId
+                        }, 1, true).then(function(data){
+                            $rootScope.$broadcast("element.updated", data, null, true);
+                        }, function(reason) {
+                            // growl.error("Getting Element Error: " + reason.message);
+                        });
                     });
                 }
             });
@@ -76,7 +85,7 @@ function StompService($rootScope, UtilsService, $window, $location, ApplicationS
     var stompFailureCallback = function(error){
         console.log('STOMP: ' + error);
         stompConnect();
-        console.log('STOMP: Reconecting in 10 seconds');
+        console.log('STOMP: Reconnecting in 10 seconds');
     };
     var stompConnect = function(){
         stompClient = Stomp.client(host);
