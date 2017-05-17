597c699b23ff603a68e1fc421c4a958b1be7cf35
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-05-17 14:12:10 -0700

    update application service test

--- test/develop/unit/ServiceSpecs/new/ApplicationServiceSpecNew.js
+++ test/develop/unit/ServiceSpecs/new/ApplicationServiceSpecNew.js
@@ -6,7 +6,6 @@ describe('Service: ApplicationService', function () {
     var ApplicationServiceObj;
     var mockURLService, mockQ, mockHttp;
     var $httpBackend, $rootScope;
-
     var root = '/alfresco/service';
 
     // beforeEach(function () {
@@ -22,45 +21,46 @@ describe('Service: ApplicationService', function () {
 
     beforeEach(inject(function ($injector) {
         ApplicationServiceObj = $injector.get('ApplicationService');
-        mockURLService        = $injector.get('URLService');
-        mockQ                 = $injector.get('$q');
-        $httpBackend          = $injector.get('$httpBackend');
-        $rootScope            = $injector.get('$rootScope');
-
-        var mmsV = {mmsVersion: "3.0.0-rc4"};
     }));
-
-    afterEach(function () {
-        $httpBackend.verifyNoOutstandingRequest();
-    });
 
     describe('Method: createUniqueId', function () {
         it('should create a unique id by retrieving the source object from ApplicationService', function () {
             var source = ApplicationServiceObj.getSource();
             expect(source).toBeDefined();
             expect(source[14]).toMatch("4");
-            // expect(source[19]).toMatch("?"); //what's this?
-            expect(source[8]).toMatch("-");
-            expect(source[13]).toMatch("-");
-            expect(source[18]).toMatch("-");
-            expect(source[23]).toMatch("-");
         });
     });
 
     describe('Method: getMmsVersion', function () {
-        var mmsVersion;
-        var mmsVersionData = "3.0.0-rc4"; 
-        $httpBackend.when('GET', root + '/mmsversion').respond(
+        beforeEach(inject(function ($injector) {
+            ApplicationServiceObj = $injector.get('ApplicationService');
+            mockURLService        = $injector.get('URLService');
+            mockQ                 = $injector.get('$q');
+            $httpBackend          = $injector.get('$httpBackend');
+            $rootScope            = $injector.get('$rootScope');
+            var mmsV = { mmsVersion: "3.0.0-rc4" };
+
+            $httpBackend.when('GET',root + '/mmsversion').respond(
             function(method, url, data) {
                 return [200, mmsV];
             });
+        }));
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+       
         it('should retrieve the mmsVersion from the application', inject(function () {
+            var mmsVersion;
+            var mmsVersionData = "3.0.0-rc4";
             ApplicationServiceObj.getMmsVersion().then(function (data) {
-                mmsVersion = data.mmsVersion;
+                mmsVersion = data;
+                // console.log('inside success',data);
             }, function (reason) {
                 mmsVersion = "Could not retrieve due to failure: " + reason.message;
+                // console.log('inside success',mmsVersion);
             });
-            $httpBackend.flush();
+            $httpBackend.flush(); 
             expect(mmsVersion).toEqual(mmsVersionData);
         }));
     });
