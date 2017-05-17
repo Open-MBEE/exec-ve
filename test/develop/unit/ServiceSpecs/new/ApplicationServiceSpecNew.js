a13f47b1740c58f199415f5bc00247cbe7ce4ec8
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-05-17 14:57:45 -0700

    Cleanup application service tests

--- test/develop/unit/ServiceSpecs/new/ApplicationServiceSpecNew.js
+++ test/develop/unit/ServiceSpecs/new/ApplicationServiceSpecNew.js
@@ -8,17 +8,6 @@ describe('Service: ApplicationService', function () {
     var $httpBackend, $rootScope;
     var root = '/alfresco/service';
 
-    // beforeEach(function () {
-    //     module(function ($provide) {
-    //         $provide.service('URLService', function () {
-    //             this.getMmsVersionURL = jasmine.createSpy('getMmsVersionURL');
-    //         });
-    //         $provide.service('$q', function () {
-    //             this.defer = jasmine.createSpy('defer');
-    //         });
-    //     });
-    // });
-
     beforeEach(inject(function ($injector) {
         ApplicationServiceObj = $injector.get('ApplicationService');
     }));
@@ -35,21 +24,21 @@ describe('Service: ApplicationService', function () {
         beforeEach(inject(function ($injector) {
             ApplicationServiceObj = $injector.get('ApplicationService');
             mockURLService        = $injector.get('URLService');
-            mockQ                 = $injector.get('$q');
             $httpBackend          = $injector.get('$httpBackend');
             $rootScope            = $injector.get('$rootScope');
             var mmsV = { mmsVersion: "3.0.0-rc4" };
 
-            $httpBackend.when('GET',root + '/mmsversion').respond(
+            $httpBackend.whenGET(root + '/mmsversion').respond(
             function(method, url, data) {
                 return [200, mmsV];
             });
         }));
+
         afterEach(function () {
-            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+     	    $httpBackend.verifyNoOutstandingRequest();
         });
 
-       
         it('should retrieve the mmsVersion from the application', inject(function () {
             var mmsVersion;
             var mmsVersionData = "3.0.0-rc4";
@@ -57,6 +46,7 @@ describe('Service: ApplicationService', function () {
                 mmsVersion = data;
                 // console.log('inside success',data);
             }, function (reason) {
+                //TODOTEST Should have a test with failure response
                 mmsVersion = "Could not retrieve due to failure: " + reason.message;
                 // console.log('inside success',mmsVersion);
             });
