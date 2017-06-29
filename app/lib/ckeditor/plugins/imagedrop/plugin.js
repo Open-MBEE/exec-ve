268cec0ec08522a58798018134e8d9f9b03dd4a3
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-06-29 10:49:42 -0700

    update image drop to only catch actual dropped data not widget drag and drop

--- app/lib/ckeditor/plugins/imagedrop/plugin.js
+++ app/lib/ckeditor/plugins/imagedrop/plugin.js
@@ -13,71 +13,73 @@
 'use strict';
 
 ( function() {
-	CKEDITOR.plugins.add( 'imagedrop', {
-		requires: 'uploadwidget',
+    CKEDITOR.plugins.add( 'imagedrop', {
+        requires: 'uploadwidget',
 
-		onLoad: function() {
-			CKEDITOR.addCss(
-				'.cke_upload_uploading img{' +
-					'opacity: 0.3' +
-				'}'
-			);
-		},
+        onLoad: function() {
+            CKEDITOR.addCss(
+                '.cke_upload_uploading img{' +
+                    'opacity: 0.3' +
+                '}'
+            );
+        },
 
-		init: function( editor ) {
-			// Do not execute this paste listener if it will not be possible to upload file.
-			if ( !CKEDITOR.plugins.clipboard.isFileApiSupported ) {
-				return;
-			}
+        init: function( editor ) {
+            // Do not execute this paste listener if it will not be possible to upload file.
+            if ( !CKEDITOR.plugins.clipboard.isFileApiSupported ) {
+                return;
+            }
 
-			var fileTools = CKEDITOR.fileTools;
+            var fileTools = CKEDITOR.fileTools;
 
-			// Handle images which are available in the dataTransfer.
-			fileTools.addUploadWidget( editor, 'imagedrop', {
-				supportedTypes: /image\/(jpeg|png|gif|bmp)/,
+            // Handle images which are available in the dataTransfer.
+            fileTools.addUploadWidget( editor, 'imagedrop', {
+                supportedTypes: /image\/(jpeg|png|gif|bmp)/,
 
-				loadMethod: 'load',
-				// uploadUrl: uploadUrl,
+                loadMethod: 'load',
+                // uploadUrl: uploadUrl,
 
-				fileToElement: function() {
-					var img = new CKEDITOR.dom.element( 'img' );
-					img.setAttribute( 'src', loadingImage );
-					return img;
-				},
+                fileToElement: function() {
+                    var img = new CKEDITOR.dom.element( 'img' );
+                    img.setAttribute( 'src', loadingImage );
+                    return img;
+                },
 
-				parts: {
-					img: 'img'
-				},
+                parts: {
+                    img: 'img'
+                },
 
-				onUploading: function( upload ) {
-					// Show the image during the upload.
-					this.parts.img.setAttribute( 'src', upload.data );
-				},
+                onUploading: function( upload ) {
+                    // Show the image during the upload.
+                    this.parts.img.setAttribute( 'src', upload.data );
+                },
 
-				onUploaded: function( upload ) {
-					// Set width and height to prevent blinking.
-					this.replaceWith( '<img src="' + upload.url + '" ' +
-						'width="' + this.parts.img.$.naturalWidth + '" ' +
-						'height="' + this.parts.img.$.naturalHeight + '">' );
-				}
-			} );
+                onUploaded: function( upload ) {
+                    // Set width and height to prevent blinking.
+                    this.replaceWith( '<img src="' + upload.url + '" ' +
+                        'width="' + this.parts.img.$.naturalWidth + '" ' +
+                        'height="' + this.parts.img.$.naturalHeight + '">' );
+                }
+            } );
 
-			editor.on("paste", onPaste, null, {editor: editor});
-		}
-	} );
+            editor.on("paste", onPaste, null, {editor: editor});
+        }
+    } );
 
 
-	function onPaste(event) {
+    function onPaste(event) {
         var editor = event.listenerData && event.listenerData.editor;
-		var $event = event.data.dataTransfer.$;
-        var clipboardData = $event.files;
-        var found = false;
-        var imageType = /^image/;
-		var img = event.data.dataValue;
+        if (event.data.dataTransfer.$) {
+            var $event = event.data.dataTransfer.$;
+            var clipboardData = $event.files;
+            var found = false;
+            var imageType = /^image/;
+            var img = event.data.dataValue;
 
-		// For performance reason do not parse data if it does not contain img tag and data attribute.
-		if ( !img.match( /<img[\s\S]+data:/i ) ) {
-            return;
+            // For performance reason do not parse data if it does not contain img tag and data attribute.
+            if ( !img.match( /<img[\s\S]+data:/i ) ) {
+                return;
+            }
         }
 
         if (!clipboardData) {
@@ -121,10 +123,10 @@
         reader.readAsDataURL(file);
     }
 
-	// jscs:disable maximumLineLength
-	// Black rectangle which is shown before image is loaded.
-	var loadingImage = 'data:image/gif;base64,R0lGODlhDgAOAIAAAAAAAP///yH5BAAAAAAALAAAAAAOAA4AAAIMhI+py+0Po5y02qsKADs=';
-	// jscs:enable maximumLineLength
+    // jscs:disable maximumLineLength
+    // Black rectangle which is shown before image is loaded.
+    var loadingImage = 'data:image/gif;base64,R0lGODlhDgAOAIAAAAAAAP///yH5BAAAAAAALAAAAAAOAA4AAAIMhI+py+0Po5y02qsKADs=';
+    // jscs:enable maximumLineLength
 
 
 } )();
