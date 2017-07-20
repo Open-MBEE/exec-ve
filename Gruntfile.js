92d1d8c782fa1899e7f01554349bb6a15fa74487
Lam, Doris T (393A) <Doris.T.Lam@jpl.nasa.gov>
2017-07-20 16:27:22 -0700

    up artifactory snapshot version

--- Gruntfile.js
+++ Gruntfile.js
@@ -286,7 +286,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: 'gov.nasa.jpl:evm:zip',
-            version: '3.0.1-SNAPSHOT',
+            version: '3.0.2-SNAPSHOT',
             path: 'deploy/'
           }]
         }
