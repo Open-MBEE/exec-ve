33ee3197d12ab1f9758ba4131478db7f29f0881c
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-08-28 11:38:02 -0700

    cleanup

--- src/directives/mmsCkEditor.js
+++ src/directives/mmsCkEditor.js
@@ -83,9 +83,6 @@ function mmsCkeditor(CacheService, ElementService, UtilsService, ViewService, $u
             $scope.cancel = function() {
                 $uibModalInstance.dismiss();
             };
-            $scope.openProposeModal = function() {
-                $uibModalInstance.close(false);
-            };
 
              // Set search result options
             $scope.searchOptions= {};
@@ -194,10 +191,6 @@ function mmsCkeditor(CacheService, ElementService, UtilsService, ViewService, $u
                 size: 'lg'
             });
             instance.result.then(function(tag) {
-                if (!tag) {
-                    proposeCallback(ed);
-                    return;
-                }
                 if (fromAutocomplete) {
                     ed.execCommand('undo');
                 }
@@ -205,18 +198,6 @@ function mmsCkeditor(CacheService, ElementService, UtilsService, ViewService, $u
             }, function() {
                 var focusManager = new CKEDITOR.focusManager( ed );
                 focusManager.focus();
-            });
-        };
-
-        var proposeCallback = function(ed) {
-            var instance = $uibModal.open({
-                template: proposeModalTemplate,
-                scope: scope,
-                controller: ['$scope', '$uibModalInstance', transcludeCtrl],
-                size: 'lg'
-            });
-            instance.result.then(function(tag) {
-                ed.insertHtml( tag );
             });
         };
 
