d4b287d68489e67cab7be2a0057aff87660084ee
Shakeh Khudikyan <Shakeh.E.Khudikyan@jpl.nasa.gov>
2017-10-09 10:10:35 -0700

    update release version in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -275,7 +275,7 @@ module.exports = function(grunt) {
     artifactory: {
       options: {
         url: artifactoryUrl,
-        repository: 'libs-snapshot-local',
+        repository: 'libs-release-local',
         username: artifactoryUser,
         password: artifactoryPassword
       },
@@ -286,7 +286,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: 'gov.nasa.jpl:evm:zip',
-            version: '3.2.0-SNAPSHOT',
+            version: '3.2.0',
             path: 'deploy/'
           }]
         }
