78ca24f10058ed76e60fb04ade7bcc1d1c28501e
ellen <ewyk@jpl.nasa.gov>
2017-11-09 09:57:52 -0800

    Changed copy of autosave messaging and added VE styling to the modals

--- app/lib/ckeditor/plugins/autosave/lang/en.js
+++ app/lib/ckeditor/plugins/autosave/lang/en.js
@@ -4,7 +4,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang('autosave', 'en', {
     dateFormat: 'LLL',
-    autoSaveMessage: 'Auto Saved',
+    autoSaveMessage: 'Saved Locally <a href="https://opencae.jpl.nasa.gov/alfresco/mmsapp/mms.html#/projects/PROJECT-ID_10_15_15_1_41_52_PM_5b84f7be_1506a83819c__6bce_cae_tw_jpl_nasa_gov_128_149_19_85/master/documents/_18_0_2_8630260_1446850132083_177552_51111/views/MMS_1506985433518_b5775631-5dce-484b-abea-2f4860a23b36" target="_blank"><i class="fa fa-info-circle" aria-hidden="true"></a>',
     loadSavedContent: 'An auto-saved version of this content from "{0}" has been found. Would you like to compare content versions and choose which one to load? Clicking Cancel will remove previously auto-saved content.',
     title: 'Compare auto-saved content with that loaded from the website',
     loadedContent: 'Loaded content', 
--- src/lib/ckeditor/plugins/autosave/plugin.js
+++ src/lib/ckeditor/plugins/autosave/plugin.js
@@ -290,14 +290,14 @@ CKEDITOR.MmsAutosavePlugin =
            '<div id=\"'+ autosaveModalId + '\" class="modal autosave">\n' +
             '    <div class="modal-content autosave">\n' +
             '        <div class="modal-header">\n' +
-            '            <h1>Do you want to load autosave content?</h1>\n' +
+            '            <h4>Recovered Version Found <a href="https://opencae.jpl.nasa.gov/alfresco/mmsapp/mms.html#/projects/PROJECT-ID_10_15_15_1_41_52_PM_5b84f7be_1506a83819c__6bce_cae_tw_jpl_nasa_gov_128_149_19_85/master/documents/_18_0_2_8630260_1446850132083_177552_51111/views/MMS_1506985433518_b5775631-5dce-484b-abea-2f4860a23b36" target="_blank"><i class="fa fa-info-circle" aria-hidden="true"></i></a></h4>\n' +
             '        </div>\n' +
             '        <div class="modal-body">\n' +
             '            <p id=\"'+ autosaveMessageId + '\">'+ message +'</p>\n' +
             '        </div>\n' +
             '        <div class="modal-footer">\n' +
-            '                <button id="autosave-confirm" class="btn btn-primary">Confirm</button>\n' +
-            '                <button id="autosave-cancel" class="btn btn-secondary">Cancel</button>\n' +
+            '                <button id="autosave-confirm" class="btn btn-primary">Review</button>\n' +
+            '                <button id="autosave-cancel" class="btn btn-tertiary">Discard</button>\n' +
             '        </div>\n' +
             '    </div>\n' +
             '</div>';
@@ -337,11 +337,27 @@ CKEDITOR.MmsAutosavePlugin =
                     }, 2000);
                 }
             } else if (messageType == "notification") {
-                var notification = new CKEDITOR.plugins.notification(editorInstance, { message: editorInstance.lang.autosave.autoSaveMessage, type: 'success' });
+                var notification = new CKEDITOR.plugins.notification(editorInstance, { message: editorInstance.lang.autosave.autoSaveMessage, type: 'success', duration: 5000 });
+                var editorElement = $(editorInstance.container['$']);
                 notification.show();
+                var autosavePopupElement = $('#' + notification.id);
+                autosavePopupElement.appendTo(editorElement);
+                editorElement.css('position', 'relative');
+
+                autosavePopupElement.css({
+                    position: 'absolute',
+                   zIndex: 5000
+                });
+                autosavePopupElement.css( {
+                    //float: 'right'
+                   // position: 'absolute',
+                   // zIndex: 5000,
+                   left:editorElement.outerWidth(true) - autosavePopupElement.outerWidth(true),
+                   top: editorElement.outerHeight(true) - autosavePopupElement.outerHeight(true)
+               } );
             }
         }
-    }
+    }   
 
     function RemoveStorage(autoSaveKey, editor) {
         if (editor.config.autosave_timeOutId) {
