f16f050f9a637fec3d64c3ab466be6d2f58c8398
Lam, Doris T (393A) <Doris.T.Lam@jpl.nasa.gov>
2018-01-08 15:45:38 -0800

    bump version

--- Gruntfile.js
+++ Gruntfile.js
@@ -296,7 +296,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: 'gov.nasa.jpl:evm:zip',
-            version: '3.2.2-SNAPSHOT',
+            version: '3.3.0-SNAPSHOT',
             path: 'deploy/'
           }]
         }
