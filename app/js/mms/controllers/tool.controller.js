c6a50717e6d751f89b7b6df5568145f9837e4cf3
Lam, Doris T (393A) <Doris.T.Lam@jpl.nasa.gov>
2018-01-10 22:25:43 -0800

    fix tree update on view reorder after navigating to another view

--- app/js/mms/controllers/tool.controller.js
+++ app/js/mms/controllers/tool.controller.js
@@ -363,7 +363,7 @@ function($scope, $rootScope, $state, $uibModal, $q, $timeout, hotkeys,
             $scope.viewContentsOrderApi.refresh();
             growl.success('Save Succesful');
             $rootScope.ve_tbApi.toggleButtonSpinner('view-reorder-save');
-            $rootScope.$broadcast('view.reorder.saved', $scope.viewId);
+            $rootScope.$broadcast('view.reorder.saved', $scope.viewOb.id);
         }, function(reason) {
             $scope.viewContentsOrderApi.refresh();
             viewSaving = false;
