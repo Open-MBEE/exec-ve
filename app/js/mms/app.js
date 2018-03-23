8b2ae67b0037f508ec16734db427bfb400c3dbc9
Shakeh Brys <Shakeh.E.Brys@jpl.nasa.gov>
2018-03-23 10:48:12 -0700

    keep ve for selection page

--- app/js/mms/app.js
+++ app/js/mms/app.js
@@ -115,7 +115,7 @@ angular.module('mmsApp', ['mms', 'mms.directives', 'app.tpls', 'fa.directive.bor
             'login@': {
                 templateUrl: 'partials/mms/select.html',
                 controller: ['$scope', '$rootScope', '$state', '$stateParams', 'orgObs', 'ProjectService', 'AuthService', 'growl', '$localStorage', function($scope, $rootScope, $state, $stateParams, orgObs, ProjectService, AuthService, growl, $localStorage) {
-                    $rootScope.ve_title = 'Projects';
+                    $rootScope.ve_title = 'View Editor';
                     $scope.fromLogin = $stateParams.fromLogin;
                     $localStorage.$default({org: orgObs[0]});
                     $scope.spin = false;
