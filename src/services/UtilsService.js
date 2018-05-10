eeb34baf3b6eab1c0f8bff9c234b32f67cc8f90a
Lam, Doris T (393A) <Doris.T.Lam@jpl.nasa.gov>
2018-05-10 08:24:08 -0700

    update css for pdf

--- src/services/UtilsService.js
+++ src/services/UtilsService.js
@@ -1051,6 +1051,7 @@ function UtilsService($q, $http, CacheService, URLService, ApplicationService, _
                 ".toc li > a[href]::after {content: leader('.') target-counter(attr(href), page);}\n" + 
                 ".tot li > a[href]::after {content: leader('.') target-counter(attr(href), page);}\n" + 
                 ".tof li > a[href]::after {content: leader('.') target-counter(attr(href), page);}\n" + 
+                "p {widow: 3; orphan: 3;}\n" + 
                 "@page {margin: 0.5in;}\n" + 
                 "@page:first {@top {content: ''} @bottom {content: ''} @top-left {content: ''} @top-right {content: ''} @bottom-left {content: ''} @bottom-right {content: ''}}\n";
                 //"@page big_table {  size: 8.5in 11in; margin: 0.75in; prince-shrink-to-fit:auto;}\n" +  //size: 11in 8.5in;
