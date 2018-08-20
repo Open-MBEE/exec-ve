dd90429ae59ebbc620f2dfd76a8a45dbe05f8418
Lam, Doris T (393A) <Doris.T.Lam@jpl.nasa.gov>
2018-08-20 13:40:30 -0700

    ignore get last known element for now until fix in 3.4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -355,7 +355,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: groupId + ':ve:zip',
-            version: '3.3.1',
+            version: '3.3.2',
             path: 'deploy/'
           }]
         }
