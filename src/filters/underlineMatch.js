288dbeca6d9141f7a3f37c9b2697700933abb295
chhay menghong <menghong.chhay@jpl.nasa.gov>
2018-10-08 15:55:11 -0700

    rename fn

--- src/filters/underlineMatch.js
+++ src/filters/underlineMatch.js
@@ -2,9 +2,9 @@
 
 angular.module('mms').filter('underlineMatch', ['$filter', partialMatch]);
 function partialMatch($filter) {
-    var standardDateFilterFn = $filter('uibTypeaheadHighlight');
+    var uibTypeaheadHighlightFn = $filter('uibTypeaheadHighlight');
     return function(match, query) {
-        var result = standardDateFilterFn(match, query);
+        var result = uibTypeaheadHighlightFn(match, query);
         result = result.replace('<strong>', '<u>').replace('</strong>', '</u>');
         return result;
     }
