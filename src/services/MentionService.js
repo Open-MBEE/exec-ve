57c71483934c6dfa0ca33f57ffcb962103757b13
chhay menghong <menghong.chhay@jpl.nasa.gov>
2018-10-08 14:25:59 -0700

    grab more information for each fast cf listing item

--- src/services/MentionService.js
+++ src/services/MentionService.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('mms')
-    .factory('MentionService', ['$rootScope', '$compile', '$timeout', 'CacheService', MentionService]);
+    .factory('MentionService', ['$rootScope', '$compile', '$timeout', 'moment', 'CacheService', 'ViewService', MentionService]);
 
-function MentionService($rootScope, $compile, $timeout, CacheService) {
+function MentionService($rootScope, $compile, $timeout, moment, CacheService, ViewService) {
     /** Used to maintain all mention in all ckeditors **/
     var mentions = {};
     var mentionPlacerHolderPrefix = 'mentionPlaceHolder';
@@ -19,10 +19,23 @@ function MentionService($rootScope, $compile, $timeout, CacheService) {
 
     function getFastCfListing(projectId, refId) {
         return CacheService.getLatestElements(projectId, refId).reduce(function(result, cacheElement) {
-            result.push({ id : cacheElement.id, name : cacheElement.name , type: 'name', label: cacheElement.name + ' - name' });
-            result.push({ id : cacheElement.id, name : cacheElement.name , type: 'doc', label: cacheElement.name + ' - documentation' });
+            var iconClass = 'pe-type-' + ViewService.getElementType(cacheElement);
+            result.push({id: cacheElement.id, name: cacheElement.name, type: 'name',
+                iconClass: iconClass, documentation: cacheElement.documentation || 'no text',
+                editor: cacheElement._modifier, editTime: moment(cacheElement._modified).fromNow(),
+                elementType: cacheElement.type
+            });
+
             if (cacheElement.type === 'Property') {
-                result.push({ id : cacheElement.id, name : cacheElement.name , type: 'val', label: cacheElement.name + ' - value' });
+                var value = String(cacheElement.defaultValue.value);
+                if (!value || value === 'undefined') {
+                    value = 'this field is empty';
+                }
+                result.push({id: cacheElement.id, name: cacheElement.name, type: 'val',
+                    iconClass: iconClass, value: value,
+                    editor: cacheElement._modifier, editTime: moment(cacheElement._modified).fromNow(),
+                    elementType: cacheElement.type
+                });
             }
             return result;
         }, []);
