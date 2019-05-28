e0a2d66c07e1642af4a438af6ad726e20f0c4570
Shakeh Brys <sekhudikyan@gmail.com>
2019-05-28 11:00:52 -0700

    update version

--- Gruntfile.js
+++ Gruntfile.js
@@ -364,7 +364,7 @@ module.exports = function(grunt) {
         options: {
           publish: [{
             id: groupId + ':ve:zip',
-            version: '3.5.2',
+            version: '3.5.4-SNAPSHOT',
             path: 'deploy/'
           }]
         }
