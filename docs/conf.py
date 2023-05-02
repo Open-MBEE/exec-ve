ec7af2185f1ace76ff1e613da09cefe5a4e2f8b1
Enquier <Enquier@users.noreply.github.com>
2023-05-01 21:00:45 -0600

    updating sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
 project = 'OpenMBEE View Editor'
-author = project+' Project Team'
-copyright = '2019, '+author
-version = '5.0'
-release = '5.0.0'
-try:
-    import sphinx_bootstrap_theme
-    html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-    html_theme = 'bootstrap'
-except:
-    pass
+copyright = '2013-2023, OpenMBEE Open Source Community'
+author = 'OpenMBEE Open Source Community'
+
+release = '0.1'
+version = '5.0.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+numfig = True
+autosectionlabel_prefix_document = True
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+
 #these are enforced by rstdoc, but keep them for sphinx-build
 numfig = 0
 smartquotes = 0
